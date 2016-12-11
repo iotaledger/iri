bcbc5de1bb49b2fadaee007e1500e46815346b48
davassi <gianluigi.davassi@gmail.com>
2016-12-11 08:38:06 +0100

    rolling back node log level

--- src/main/java/com/iota/iri/service/Node.java
+++ src/main/java/com/iota/iri/service/Node.java
@@ -126,7 +126,7 @@ public class Node {
                                     	}
                                     }
                                 } catch (final RuntimeException e) {
-                                	log.error("Received an Invalid Transaction. Dropping it...");
+                                    log.error("Received an Invalid Transaction. Dropping it...", e);
                                     neighbor.incInvalidTransactions();
                                 }
                                 break;

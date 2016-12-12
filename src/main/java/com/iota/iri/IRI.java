702d85730b5a6ce0881e990d1bffde39cbeeb9af
davassi <gianluigi.davassi@gmail.com>
2016-12-12 19:38:54 +0100

    1.1.2.1

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -29,7 +29,7 @@ public class IRI {
     private static final Logger log = LoggerFactory.getLogger(IRI.class);
 
     public static final String NAME = "IRI";
-    public static final String VERSION = "1.1.2.1";
+    public static final String VERSION = "1.1.2.2";
 
     public static void main(final String[] args) {
 

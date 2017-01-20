56c4d673a650be6ebfa8fcc16fcd14b1b192e07a
Alfred Keller <alfred.keller@hitron.ch>
2017-01-20 15:36:03 +0100

    New version number for "Latest Milestone Tracker" bugfix.

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -30,7 +30,7 @@ public class IRI {
     private static final Logger log = LoggerFactory.getLogger(IRI.class);
 
     public static final String NAME = "IRI Testnet";
-    public static final String VERSION = "1.1.2.4";
+    public static final String VERSION = "1.1.2.4-1";
 
     public static void main(final String[] args) {
 

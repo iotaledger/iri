214c7be44be68dd5bf587f543638f41a93aaa4e6
Alfred Keller <alfred.keller@hitron.ch>
2017-01-26 12:00:34 +0100

    missing import

--- src/main/java/com/iota/iri/service/TipsManager.java
+++ src/main/java/com/iota/iri/service/TipsManager.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

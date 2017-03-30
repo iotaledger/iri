e07c1899fd2355d6fcc48adb8c1bc091c98e8c66
Paul D Handy <paul.d.handy@gmail.com>
2017-03-30 15:28:11 -0600

    updated tags to 1.1.3.4

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -35,7 +35,7 @@ public class IRI {
     private static final Logger log = LoggerFactory.getLogger(IRI.class);
 
     public static final String NAME = "IRI Testnet";
-    public static final String VERSION = "1.1.3.3";
+    public static final String VERSION = "1.1.3.4";
 
     public static void main(final String[] args) throws IOException {
 

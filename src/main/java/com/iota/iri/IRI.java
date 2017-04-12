d541fdafecea71d0fbac1ecf0caf8be83beef4fa
Paul D Handy <paul.d.handy@gmail.com>
2017-04-11 23:14:08 -0600

    v1.1.3.6 for Persistent Milestone Snapshot

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -41,7 +41,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
     public static final String TESTNET_NAME = "IRI Testnet";
-    public static final String VERSION = "1.1.3.5";
+    public static final String VERSION = "1.1.3.6";
 
     public static void main(final String[] args) throws IOException {
 

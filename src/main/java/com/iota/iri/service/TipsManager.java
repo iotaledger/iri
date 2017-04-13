31f4a71d0acc3d258863cf958c7a848cd17ab575
Paul D Handy <paul.d.handy@gmail.com>
2017-04-13 17:43:57 -0600

    Walk through all transactions, no matter if they are broken bundles or not

--- src/main/java/com/iota/iri/service/TipsManager.java
+++ src/main/java/com/iota/iri/service/TipsManager.java
@@ -217,10 +217,10 @@ public class TipsManager {
 
             Map<Hash, Integer> ratings = new HashMap<>();
             Set<Hash> analyzedTips = new HashSet<>();
-            Set<Hash> visitedTips = new HashSet<>();
             try {
                 int traversedTails = 0;
                 Hash tip = preferableMilestone;
+                Hash tail = tip;
                 if (extraTip != null) {
                     TransactionViewModel transactionViewModel = TransactionViewModel.fromHash(tip);
                     while (milestoneDepth-- > 0 && !tip.equals(Hash.NULL_HASH)) {
@@ -260,7 +260,7 @@ public class TipsManager {
                             break;
                         }
                     }
-                    transactionViewModel = TransactionViewModel.fromHash(tips[carlo]).getBundle().getTail();
+                    transactionViewModel = TransactionViewModel.fromHash(tips[carlo]);
                     if (transactionViewModel == null) {
                         break;
                     } else if (!(checkSolidity(transactionViewModel.getHash()) && updateFromSnapshot(transactionViewModel.getHash()))) {
@@ -268,16 +268,15 @@ public class TipsManager {
                     } else if (transactionViewModel.getHash().equals(extraTip) || transactionViewModel.getHash().equals(tip)) {
                         break;
                     } else {
-                        if(visitedTips.add(transactionViewModel.getHash())) {
-                            traversedTails++;
-                            tip = transactionViewModel.getHash();
-                        } else {
-                            break;
+                        traversedTails++;
+                        tip = transactionViewModel.getHash();
+                        if(transactionViewModel.getCurrentIndex() == 0) {
+                            tail = tip;
                         }
                     }
                 }
                 log.info("Tails traversed: " + traversedTails);
-                return tip;
+                return tail;
             } catch (Exception e) {
                 e.printStackTrace();
                 log.error("Encountered error: " + e.getLocalizedMessage());

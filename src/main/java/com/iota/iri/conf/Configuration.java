84026a07399e088854ad44a23191f9c8880ee00d
Paul D Handy <paul.d.handy@gmail.com>
2017-04-19 10:34:27 -0600

    changed default drop probability

--- src/main/java/com/iota/iri/conf/Configuration.java
+++ src/main/java/com/iota/iri/conf/Configuration.java
@@ -66,7 +66,7 @@ public class Configuration {
         conf.put(DefaultConfSettings.P_REMOVE_REQUEST.name(), "0.1");
         conf.put(DefaultConfSettings.P_DROP_TRANSACTION.name(), "0.0");
         conf.put(DefaultConfSettings.P_SELECT_MILESTONE_CHILD.name(), "0.7");
-        conf.put(DefaultConfSettings.P_DROP_RANDOM_REQUEST.name(), "0.5");
+        conf.put(DefaultConfSettings.P_DROP_RANDOM_REQUEST.name(), "0.128");
         conf.put(DefaultConfSettings.EXPORT.name(), "false");
     }
 

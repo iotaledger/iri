5f8cb1efcf3d60bd87db18ccdf3fb8a92922f499
Alfred Keller <alfred.keller@hitron.ch>
2017-05-10 20:36:44 +0200

    Initial commit of 2 files.

---
+++ src/main/java/com/iota/iri/utils/MapIdentityManager.java
@@ -0,0 +1,96 @@
+package com.iota.iri.utils;
+
+import io.undertow.security.idm.IdentityManager;
+
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.Credential;
+import io.undertow.security.idm.PasswordCredential;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import com.iota.iri.hash.Curl;
+
+public class MapIdentityManager implements IdentityManager {
+
+    private final Map<String, char[]> users;
+
+    public MapIdentityManager(final Map<String, char[]> users) {
+        this.users = users;
+    }
+
+    @Override
+    public Account verify(Account account) {
+        // An existing account so for testing assume still valid.
+        return account;
+    }
+
+    @Override
+    public Account verify(String id, Credential credential) {
+        Account account = getAccount(id);
+        if (account != null && verifyCredential(account, credential)) {
+            return account;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Account verify(Credential credential) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private boolean verifyCredential(Account account, Credential credential) {
+        if (credential instanceof PasswordCredential) {
+            char[] givenPassword = ((PasswordCredential) credential).getPassword();            
+            String trytes = Converter.asciiToTrytes(new String(givenPassword));
+            int[] in_trits = Converter.trits(trytes);
+            int[] hash_trits = new int[Curl.HASH_LENGTH];
+            Curl curl;
+            curl = new Curl();
+            curl.absorb(in_trits, 0, in_trits.length);
+            curl.squeeze(hash_trits, 0, Curl.HASH_LENGTH);
+            String out_trytes = Converter.trytes(hash_trits);
+            char[] char_out_trytes = out_trytes.toCharArray();
+            char[] expectedPassword = users.get(account.getPrincipal().getName()); 
+            boolean verified = Arrays.equals(givenPassword, expectedPassword);
+            // Password can either be clear text or the hash of the password
+            if (!verified) {
+                verified = Arrays.equals(char_out_trytes, expectedPassword);
+            }            
+            return verified;
+        }
+        return false;
+    }
+
+    private Account getAccount(final String id) {
+        if (users.containsKey(id)) {
+            return new Account() {
+
+                private final Principal principal = new Principal() {
+
+                    @Override
+                    public String getName() {
+                        return id;
+                    }
+                };
+
+                @Override
+                public Principal getPrincipal() {
+                    return principal;
+                }
+
+                @Override
+                public Set<String> getRoles() {
+                    return Collections.emptySet();
+                }
+
+            };
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/iota/iri/utils/SlackBotFeed.java
@@ -0,0 +1,44 @@
+package com.iota.iri.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class SlackBotFeed {
+
+    public static void reportToSlack(final String message) {
+
+        try {
+
+            final String request = "token=" 
+                    + URLEncoder.encode("<botToken>", "UTF-8") + "&channel=" 
+                    + URLEncoder.encode("#botbox", "UTF-8") + "&text=" + URLEncoder.encode(message, "UTF-8") + "&as_user=true";
+
+            final HttpURLConnection connection = (HttpsURLConnection) (new URL("https://slack.com/api/chat.postMessage")).openConnection();
+            ((HttpsURLConnection)connection).setHostnameVerifier((hostname, session) -> true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            OutputStream out = connection.getOutputStream();
+            out.write(request.getBytes("UTF-8"));
+            out.close();
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            InputStream inputStream = connection.getInputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+
+        } catch (final Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+    
+}

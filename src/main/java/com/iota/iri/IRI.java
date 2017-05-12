894ced1c8e1126e2859450e80832f6e26f57a851
Alfred Keller <alfred.keller@hitron.ch>
2017-05-12 10:50:40 +0200

    Bumped version # to 1.1.3.8

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -40,7 +40,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
     public static final String TESTNET_NAME = "IRI Testnet";
-    public static final String VERSION = "1.1.3.7";
+    public static final String VERSION = "1.1.3.8";
 
     public static void main(final String[] args) throws IOException {
 

e43a29fd11771d0dfda8669d80b7f2dbb66b0025
Paul D Handy <paul.d.handy@gmail.com>
2017-05-17 10:08:06 -0600

    moved addressMatch to inside for loop to prevent sending to all neighbors

--- src/main/java/com/iota/iri/network/Node.java
+++ src/main/java/com/iota/iri/network/Node.java
@@ -179,8 +179,8 @@ public class Node {
     public void preProcessReceivedData(byte[] receivedData, SocketAddress senderAddress, String uriScheme) {
         TransactionViewModel receivedTransactionViewModel = null;
 
-        boolean addressMatch = false;
         for (final Neighbor neighbor : getNeighbors()) {
+            boolean addressMatch = false;
 
             if (neighbor instanceof TCPNeighbor) {
                 if (senderAddress.toString().contains(neighbor.getHostAddress())) addressMatch = true;

f8be2a986ddeb96939c4698bc0e5626df0290617
Paul D Handy <paul.d.handy@gmail.com>
2017-05-19 15:25:00 -0600

    added api init in integration test

--- src/test/java/com/iota/iri/NodeIntegrationTests.java
+++ src/test/java/com/iota/iri/NodeIntegrationTests.java
@@ -33,11 +33,16 @@ public class NodeIntegrationTests {
             folders[i*2 + 1] = new TemporaryFolder();
             iotaNodes[i] = newNode(i, folders[i*2], folders[i*2+1]);
         }
-
         Node.uri("udp://localhost:14701").ifPresent(uri -> iotaNodes[0].node.addNeighbor(iotaNodes[0].node.newNeighbor(uri, true)));
         Node.uri("udp://localhost:14700").ifPresent(uri -> iotaNodes[1].node.addNeighbor(iotaNodes[1].node.newNeighbor(uri, true)));
 
+        Thread.sleep(1000);
+        /*
+        TODO: Put some test stuff here
+         */
+
         for(Iota node: iotaNodes) {
+            node.api.shutDown();
             node.shutdown();
         }
         for(TemporaryFolder folder: folders) {
@@ -50,6 +55,7 @@ public class NodeIntegrationTests {
         log.create();
         Configuration conf = new Configuration();
         Iota iota;
+        conf.put(Configuration.DefaultConfSettings.PORT, String.valueOf(14800 + index));
         conf.put(Configuration.DefaultConfSettings.UDP_RECEIVER_PORT, String.valueOf(14700 + index));
         conf.put(Configuration.DefaultConfSettings.TCP_RECEIVER_PORT, String.valueOf(14700 + index));
         conf.put(Configuration.DefaultConfSettings.DB_PATH, db.getRoot().getAbsolutePath());
@@ -57,6 +63,7 @@ public class NodeIntegrationTests {
         conf.put(Configuration.DefaultConfSettings.TESTNET, "true");
         iota = new Iota(conf);
         iota.init();
+        iota.api.init();
         return iota;
     }
 }

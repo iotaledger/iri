00e8f2a7cd1b1ac0a850be6794221b09ffe2f238
Paul D Handy <paul.d.handy@gmail.com>
2017-05-20 09:55:30 -0600

    Updated to 1.1.3.10

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM java:8
 
-CMD ["/usr/bin/java", "-jar", "iri-1.1.3.9.jar"]
+CMD ["/usr/bin/java", "-jar", "iri-1.1.3.10.jar"]
--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -40,7 +40,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
     public static final String TESTNET_NAME = "IRI Testnet";
-    public static final String VERSION = "1.1.3.9";
+    public static final String VERSION = "1.1.3.10";
     public static Iota iota;
     public static API api;
     public static IXI ixi;

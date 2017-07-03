ea82a617753c1c9e91771ef0eec7c294f06ca261
alon-e <alon.elmaliah@gmail.com>
2017-07-03 14:18:16 +0300

    revert rating changes in prev commit

--- src/main/java/com/iota/iri/service/TipsManager.java
+++ src/main/java/com/iota/iri/service/TipsManager.java
@@ -257,11 +257,8 @@ public class TipsManager {
                 }
             }
             if(!addedBack && analyzedTips.add(currentHash)) {
-                long rating = 0;
-                if (transactionViewModel.isSolid() || true) {
-                    rating = ((extraTip != null && ledgerValidator.isApproved(currentHash))? 0: 1) + approvers.stream().map(ratings::get).filter(Objects::nonNull)
-                            .reduce((a, b) -> capSum(a,b, Long.MAX_VALUE/2)).orElse(0L);
-                }
+                long rating = (extraTip != null && ledgerValidator.isApproved(currentHash)? 0: 1) + approvers.stream().map(ratings::get).filter(Objects::nonNull)
+                        .reduce((a, b) -> capSum(a,b, Long.MAX_VALUE/2)).orElse(0L);
                 ratings.put(currentHash, rating);
             }
         }

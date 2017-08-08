6ce238231ee883dcc1069626cf61b63f3efb8c01
Paul D Handy <paul.d.handy@gmail.com>
2017-08-08 13:05:57 -0600

    Updated milestone start index and timestamp value

--- src/main/java/com/iota/iri/TransactionValidator.java
+++ src/main/java/com/iota/iri/TransactionValidator.java
@@ -70,7 +70,7 @@ public class TransactionValidator {
 
     private static void runValidation(TransactionViewModel transactionViewModel, final int minWeightMagnitude) {
         transactionViewModel.setMetadata();
-        if(transactionViewModel.getTimestamp() < 1497031200 && !transactionViewModel.getHash().equals(Hash.NULL_HASH)) {
+        if(transactionViewModel.getTimestamp() < 1502226000 && !transactionViewModel.getHash().equals(Hash.NULL_HASH)) {
             throw new RuntimeException("Invalid transaction timestamp.");
         }
 

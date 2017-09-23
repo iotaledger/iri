3ce0b5bbf737c37ce721af5bfa1ae7f246cbb2ae
Paul D Handy <paul.d.handy@gmail.com>
2017-09-22 18:08:18 -0600

    added snapshot

--- src/main/java/com/iota/iri/Snapshot.java
+++ src/main/java/com/iota/iri/Snapshot.java
@@ -20,7 +20,9 @@ import java.util.stream.Collectors;
 
 public class Snapshot {
     private static final Logger log = LoggerFactory.getLogger(Snapshot.class);
-    private static String snapshotPubKey = "BRUTUMFFJVCXEPKHZBKBHWDKARQEYCUVSCUUESJWBSSAMHAWRVHZZGROIIOETMWDKRFODD9NMC9TPOWGD";
+    private static String SNAPSHOT_PUBKEY = "ETSYRXPKSCTJAZIJZDVJTQOILVEPHGV9PHPFLJVUFQRPXGNWPDBAKHCWPPEXPCZDIGPJDQGHVIQHQYQDW";
+    private static int SNAPSHOT_PUBKEY_DEPTH = 6;
+    private static int SNAPSHOT_INDEX = 0;
 
     public static final Map<Hash, Long> initialState = new HashMap<>();
     public static final Snapshot initialSnapshot;
@@ -52,15 +54,19 @@ public class Snapshot {
                 curl.squeeze(trits, 0, Curl.HASH_LENGTH);
                 int[] digests = new int[0];
                 int[] bundle = ISS.normalizedBundle(trits);
-                int i = 0;
+                int[] root = null;
+                int i;
                 in = Snapshot.class.getResourceAsStream("/Snapshot.sig");
                 reader = new BufferedReader(new InputStreamReader(in));
-                while((line = reader.readLine()) != null) {
+                for(i = 0; i < 3 && (line = reader.readLine()) != null; i++) {
                     digests = ArrayUtils.addAll(digests, ISS.digest(SpongeFactory.Mode.KERL, Arrays.copyOfRange(bundle, i*ISS.NORMALIZED_FRAGMENT_LENGTH, (i+1)*ISS.NORMALIZED_FRAGMENT_LENGTH), Converter.trits(line)));
-                    i++;
                 }
-
-                if(!Arrays.equals(Converter.trits(snapshotPubKey), ISS.address(SpongeFactory.Mode.KERL, digests))) {
+                if((line = reader.readLine()) != null) {
+                    root = ISS.getMerkleRoot(SpongeFactory.Mode.CURLP81, ISS.address(SpongeFactory.Mode.KERL, digests), Converter.trits(line), 0, SNAPSHOT_INDEX, SNAPSHOT_PUBKEY_DEPTH);
+                } else {
+                    root = ISS.address(SpongeFactory.Mode.KERL, digests);
+                }
+                if(!Arrays.equals(Converter.trits(SNAPSHOT_PUBKEY), root)) {
                     throw new RuntimeException("Snapshot signature failed.");
                 }
             }

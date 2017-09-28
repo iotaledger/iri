3e904aa98ebbf725af86a29940397647a0479618
Scott Foster <scott@shellbit.com>
2017-09-27 23:31:58 -0700

    Added REMOTE Conf Setting Condition Test

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -182,7 +182,8 @@ public class IRI {
             configuration.put(DefaultConfSettings.TCP_RECEIVER_PORT, vrporttcp);
         }
 
-        if (parser.getOptionValue(remote) != null) {
+	final Boolean remoteBinding = configuration.getBooling(DefaultConfSetting.REMOTE);
+        if (parser.getOptionValue(remote) != null || remoteBinding ) {
             log.info("Remote access enabled. Binding API socket to listen any interface.");
             configuration.put(DefaultConfSettings.API_HOST, "0.0.0.0");
         }

b0c29c58804169ede014278e3ebf6f786c6d2fcb
Gal Rogozinski <galrogozinski@gmail.com>
2017-11-16 14:14:05 +0200

    add split() and canSplit() to persistable models
    read now appends bytes in Hashes

--- src/main/java/com/iota/iri/storage/Persistable.java
+++ src/main/java/com/iota/iri/storage/Persistable.java
@@ -1,6 +1,8 @@
 package com.iota.iri.storage;
 
+import javax.naming.OperationNotSupportedException;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by paul on 5/6/17.
@@ -11,4 +13,16 @@ public interface Persistable extends Serializable {
     byte[] metadata();
     void readMetadata(byte[] bytes);
     boolean merge();
+
+    /**
+     * @return true if the {@link Persistable} object can be splitBytes to multiple byte arrays.
+     */
+    boolean isSplittable();
+
+    /**
+     * @return a list of byte arrays that represent the serialized object.
+     * @throws OperationNotSupportedException if {@link #isSplittable()} returns false.
+     * @param maxByteLength
+     */
+    List<byte[]> splitBytes(int maxByteLength) throws OperationNotSupportedException;
 }

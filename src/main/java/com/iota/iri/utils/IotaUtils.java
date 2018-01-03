6393424ee28662cf4e2466920e99f531e2245bd9
Gal Rogozinski <galrogozinski@gmail.com>
2018-01-03 19:35:03 +0200

    iota utils

--- src/main/java/com/iota/iri/utils/IotaUtils.java
+++ src/main/java/com/iota/iri/utils/IotaUtils.java
@@ -18,21 +18,23 @@ public class IotaUtils {
 
 
     /**
+     * Increment the array in place
+     *
      * @param array byte array
      * @return the array above with the integer represented by the last 4 bytes incremented by 1
      * @throws Exception in case of overflow
      */
-    public static byte[] incrementArrayIntegerSuffix(byte[] array) throws Exception {
+    public static byte[] incrementArrayIntegerSuffix(byte[] array) throws OverFlowException {
         return advanceByteArray(array, 3, array.length - 1);
     }
 
-    private static byte[] advanceByteArray(byte[] array, int remainingCarryPasses, int currentIndexToAdvance) throws Exception {
+    private static byte[] advanceByteArray(byte[] array, int remainingCarryPasses, int currentIndexToAdvance) throws OverFlowException {
         if (array[currentIndexToAdvance] == Byte.MAX_VALUE) {
             if (remainingCarryPasses == 0) {
-                throw new Exception("Can't advance the byte array without superseding allowed carry");
+                throw new OverFlowException("Can't advance the byte array without superseding allowed carry");
             }
             if (currentIndexToAdvance == 0) {
-                throw new Exception("Can't advance the byte array without overflowing");
+                throw new OverFlowException("Can't advance the byte array without overflowing");
             }
             advanceByteArray(array, --remainingCarryPasses, --currentIndexToAdvance);
         }
@@ -40,4 +42,24 @@ public class IotaUtils {
         return array;
     }
 
+    public static class OverFlowException extends Exception{
+        public OverFlowException() {
+        }
+
+        public OverFlowException(String message) {
+            super(message);
+        }
+
+        public OverFlowException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public OverFlowException(Throwable cause) {
+            super(cause);
+        }
+
+        public OverFlowException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+            super(message, cause, enableSuppression, writableStackTrace);
+        }
+    };
 }

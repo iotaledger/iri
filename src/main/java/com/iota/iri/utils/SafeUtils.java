006c04730274d430ed914d0d468a0ad8088fd862
galrogo <gal.rogozinski@iota.org>
2018-01-14 16:13:22 +0200

    create safeutils

---
+++ src/main/java/com/iota/iri/utils/SafeUtils.java
@@ -0,0 +1,17 @@
+package com.iota.iri.utils;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+
+/**
+ * Null safe utils
+ *
+ * @author galrogo
+ */
+public class SafeUtils {
+
+    public static <T> boolean isContaining(Collection<T> collection, T element) {
+        return collection != null && element != null && collection.contains(element);
+    }
+}

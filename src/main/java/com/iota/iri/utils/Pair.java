a1e4de308e2fbe3949a8a764a5aabbff23f9de65
footloosejava <32090281+footloosejava@users.noreply.github.com>
2018-03-28 17:13:28 -0700

    RocksDBPersistenceProvider - lots of edits - no breaking changes. * Centralized repeat code * Marked unused code. * Ensure that all sets returned are unmodifiable. * Made sure that all database iterators are closed in final method or that they are closed in try-with-resources blocks. * Removed unneeded AtomicReference for maps. * Added logging for failed mkdir. * + some misc cleanup editing.
    Pair.java - made all values final.
    
    Quietly.java - can be used anywhere to quietly close resources without interrupting further closings or burying already thrown exceptions.
    
    Signed-off-by: footloosejava <32090281+footloosejava@users.noreply.github.com>

--- src/main/java/com/iota/iri/utils/Pair.java
+++ src/main/java/com/iota/iri/utils/Pair.java
@@ -2,10 +2,12 @@ package com.iota.iri.utils;
 
 /**
  * Created by paul on 4/15/17.
+ * Edited by footloosejava 03/28/2018
  */
 public class Pair<S, T> {
-    public S low;
-    public T hi;
+    public final S low;
+    public final T hi;
+
     public Pair(S k, T v) {
         low = k;
         hi = v;

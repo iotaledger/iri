baa9dddec53b9b4f2f2da91da2d169845cd59b4a
footloosejava <32090281+footloosejava@users.noreply.github.com>
2018-04-05 14:10:38 -0700

    Kerl performance optimizations:  - using a cache of function powers  - avoiding array range copy  - misc optimizations  Across the board 30%+ improvement in Kerl speed during absorb.  Up to 15% to 20% improvement in Kerl speed in squeeze.
    Signed-off-by: footloosejava <32090281+footloosejava@users.noreply.github.com>

--- src/main/java/com/iota/iri/hash/Kerl.java
+++ src/main/java/com/iota/iri/hash/Kerl.java
@@ -11,8 +11,15 @@ public class Kerl implements Sponge {
     public static final int BIT_HASH_LENGTH = 384;
     public static final int BYTE_HASH_LENGTH = BIT_HASH_LENGTH / 8;
 
-    private byte[] byte_state;
-    private int[] trit_state;
+    // radix constant conversion for fast math
+    public static final BigInteger RADIX = BigInteger.valueOf(Converter.RADIX);
+    // lookups for fast math
+    private static final BigInteger[] RADIX_POWERS = makeRadixPowers();
+    private static final BigInteger[] RADIX_POSITIVE_ONE_FUNCTION = makeRadixFunctionPowers(true);
+    private static final BigInteger[] RADIX_NEGATIVE_ONE_FUNCTION = makeRadixFunctionPowers(false);
+
+    private final byte[] byte_state;
+    private final int[] trit_state;
     private final Keccak.Digest384 keccak;
 
     protected Kerl() {
@@ -58,24 +65,23 @@ public class Kerl implements Sponge {
         }
 
         try {
-          do {
-              this.keccak.digest(byte_state, 0, BYTE_HASH_LENGTH);
-              //convert to trits
-              tritsFromBigInt(bigIntFromBytes(byte_state, 0, BYTE_HASH_LENGTH), trit_state, 0, Sponge.HASH_LENGTH);
+            do {
+                this.keccak.digest(byte_state, 0, BYTE_HASH_LENGTH);
+                //convert to trits
+                tritsFromBigInt(bigIntFromBytes(byte_state, 0, BYTE_HASH_LENGTH), trit_state, 0, Sponge.HASH_LENGTH);
 
-              //copy with offset
-              trit_state[HASH_LENGTH - 1] = 0;
-              System.arraycopy(trit_state, 0, trits, offset, HASH_LENGTH);
+                //copy with offset
+                trit_state[HASH_LENGTH - 1] = 0;
+                System.arraycopy(trit_state, 0, trits, offset, HASH_LENGTH);
 
-              //calculate hash again
-              for (int i = byte_state.length; i-- > 0; ) {
+                //calculate hash again
+                for (int i = byte_state.length; i-- > 0; ) {
+                    byte_state[i] = (byte) (byte_state[i] ^ 0xFF);
+                }
+                keccak.update(byte_state);
+                offset += HASH_LENGTH;
 
-                  byte_state[i] = (byte) (byte_state[i] ^ 0xFF);
-              }
-              keccak.update(byte_state);
-              offset += HASH_LENGTH;
-
-          } while ((length -= HASH_LENGTH) > 0);
+            } while ((length -= HASH_LENGTH) > 0);
         } catch (DigestException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -85,63 +91,108 @@ public class Kerl implements Sponge {
     public static BigInteger bigIntFromTrits(final int[] trits, final int offset, final int size) {
 
         BigInteger value = BigInteger.ZERO;
-
-        for (int i = size; i-- > 0; ) {
-
-            value = value.multiply(BigInteger.valueOf(Converter.RADIX)).add(BigInteger.valueOf(trits[offset + i]));
+        int power = 0;
+        for (int i = offset + size - 1; i >= offset; i--) {
+            switch (trits[i]) {
+                case 1: {
+                    int sequence = 0;
+                    while (i - 1 >= offset && trits[i - 1] == 1) {
+                        i--;
+                        sequence++;
+                    }
+                    value = value.multiply(RADIX_POWERS[power + 1 + sequence]).add(RADIX_POSITIVE_ONE_FUNCTION[sequence]);
+                    power = 0;
+                }
+                break;
+                case -1: {
+                    int sequence = 0;
+                    while (i - 1 >= offset && trits[i - 1] == -1) {
+                        i--;
+                        sequence++;
+                    }
+                    value = value.multiply(RADIX_POWERS[power + 1 + sequence]).add(RADIX_NEGATIVE_ONE_FUNCTION[sequence]);
+                    power = 0;
+                }
+                break;
+                default:
+                    power++;
+            }
         }
-
+        // do leftover pow
+        if (power > 0) {
+            value = value.multiply(RADIX_POWERS[power]);
+        }
         return value;
     }
 
     public static BigInteger bigIntFromBytes(final byte[] bytes, final int offset, final int size) {
-
-        return new BigInteger(Arrays.copyOfRange(bytes, offset, offset + size));
+        return (bytes.length == offset + size) ? new BigInteger(bytes)
+            : new BigInteger(Arrays.copyOfRange(bytes, offset, offset + size));
     }
 
     public static void tritsFromBigInt(final BigInteger value, int[] destination, int offset, int size) {
 
-        if(destination.length - offset < size) {
+        if (destination.length - offset < size) {
             throw new IllegalArgumentException("Destination array has invalid size");
         }
 
-        BigInteger absoluteValue = value.compareTo(BigInteger.ZERO) < 0 ? value.negate() : value;
+        final int signum = value.signum();
+        if (signum == 0) {
+            Arrays.fill(destination, offset, size, 0);
+            return;
+        }
+
+        BigInteger absoluteValue = value.abs();
         for (int i = 0; i < size; i++) {
 
-            BigInteger[] divRemainder = absoluteValue.divideAndRemainder(BigInteger.valueOf(Converter.RADIX));
+            BigInteger[] divRemainder = absoluteValue.divideAndRemainder(RADIX);
             int remainder = divRemainder[1].intValue();
             absoluteValue = divRemainder[0];
 
             if (remainder > Converter.MAX_TRIT_VALUE) {
-
                 remainder = Converter.MIN_TRIT_VALUE;
                 absoluteValue = absoluteValue.add(BigInteger.ONE);
             }
-            destination[offset  + i] = remainder;
-        }
-
-        if (value.compareTo(BigInteger.ZERO) < 0) {
-
-            for (int i = 0; i < size; i++) {
-
-                destination[offset + i] = -destination[offset + i];
-            }
+            destination[offset + i] = signum < 0 ? -remainder : remainder;
         }
     }
+
     public static void bytesFromBigInt(final BigInteger value, byte[] destination, int offset) {
-        if(destination.length - offset < BYTE_HASH_LENGTH) {
+        if (destination.length - offset < BYTE_HASH_LENGTH) {
             throw new IllegalArgumentException("Destination array has invalid size.");
         }
 
         final byte[] bytes = value.toByteArray();
+        final byte bVal = (byte) (bytes[0] < 0 ? -1 : 0);
         int i = 0;
         while (i + bytes.length < BYTE_HASH_LENGTH) {
 
-            destination[i++] = (byte) (bytes[0] < 0 ? -1 : 0);
+            destination[i++] = bVal;
         }
         for (int j = bytes.length; j-- > 0; ) {
 
             destination[i++] = bytes[bytes.length - 1 - j];
         }
+    }
+
+    private static BigInteger[] makeRadixPowers() {
+        BigInteger[] pows = new BigInteger[Sponge.HASH_LENGTH + 1];
+        for (int n = 0; n < pows.length; n++) {
+            pows[n] = RADIX.pow(n);
+        }
+        return pows;
+    }
+
+    private static BigInteger[] makeRadixFunctionPowers(boolean positive) {
+        BigInteger[] fns = new BigInteger[Sponge.HASH_LENGTH + 1];
+        fns[0] = positive ? BigInteger.ONE : BigInteger.ONE.negate();
+        for (int n = 1; n < fns.length; n++) {
+            BigInteger value = positive
+                ? RADIX.pow(n).add(fns[n - 1])
+                : RADIX.pow(n).negate().add(fns[n - 1]);
+            fns[n] = value;
+        }
+        return fns;
+
     }
 }

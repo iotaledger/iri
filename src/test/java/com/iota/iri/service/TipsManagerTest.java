924f89f01d1d2058931c0c2991ca5b40db0521d4
footloosejava <footloosejava@gmail.com>
2018-05-06 21:48:37 -0700

    rebased to post IOTA snapshot end of April and then merged in changes from editSnapshot previous work on PR.
    Signed-off-by: footloosejava <32090281+footloosejava@users.noreply.github.com>

--- src/test/java/com/iota/iri/service/TipsManagerTest.java
+++ src/test/java/com/iota/iri/service/TipsManagerTest.java
@@ -20,9 +20,7 @@ import org.junit.rules.TemporaryFolder;
 
 import java.util.*;
 
-import static com.iota.iri.controllers.TransactionViewModelTest.getRandomTransactionHash;
-import static com.iota.iri.controllers.TransactionViewModelTest.getRandomTransactionTrits;
-import static com.iota.iri.controllers.TransactionViewModelTest.getRandomTransactionWithTrunkAndBranch;
+import static com.iota.iri.controllers.TransactionViewModelTest.*;
 
 /**
  * Created by paul on 4/27/17.
@@ -63,8 +61,9 @@ public class TipsManagerTest {
                 messageQ, Long.parseLong(Configuration.GLOBAL_SNAPSHOT_TIME));
         int milestoneStartIndex = Integer.parseInt(Configuration.MAINNET_MILESTONE_START_INDEX);
         int numOfKeysInMilestone = Integer.parseInt(Configuration.MAINNET_NUM_KEYS_IN_MILESTONE);
-        Milestone milestone = new Milestone(tangle, Hash.NULL_HASH, Snapshot.init(
-                Configuration.MAINNET_SNAPSHOT_FILE, Configuration.MAINNET_SNAPSHOT_SIG_FILE, false).clone(),
+        Snapshot initialSnapshot = Snapshot.init(Configuration.MAINNET_SNAPSHOT_FILE, Configuration.MAINNET_SNAPSHOT_SIG_FILE, false)
+            .copySnapshot();
+        Milestone milestone = new Milestone(tangle, Hash.NULL_HASH, initialSnapshot,
                 transactionValidator, false, messageQ, numOfKeysInMilestone,
                 milestoneStartIndex, true);
         LedgerValidator ledgerValidator = new LedgerValidator(tangle, milestone, transactionRequester, messageQ);
@@ -123,7 +122,7 @@ public class TipsManagerTest {
         transaction4.store(tangle);
         Map<Hash, Long> ratings = new HashMap<>();
         tipsManager.recursiveUpdateRatings(transaction.getHash(), ratings, new HashSet<>());
-        Assert.assertTrue(ratings.get(transaction.getHash()).equals(5L));
+        Assert.assertEquals(5L, (long) ratings.get(transaction.getHash()));
     }
 
     @Test
@@ -137,7 +136,7 @@ public class TipsManagerTest {
         }
         Map<Hash, Long> ratings = new HashMap<>();
         tipsManager.recursiveUpdateRatings(hashes[0], ratings, new HashSet<>());
-        Assert.assertTrue(ratings.get(hashes[0]).equals(5L));
+        Assert.assertEquals(5L, (long) ratings.get(hashes[0]));
     }
 
     @Test
@@ -151,7 +150,7 @@ public class TipsManagerTest {
         }
         Map<Hash, Long> ratings = new HashMap<>();
         tipsManager.recursiveUpdateRatings(hashes[0], ratings, new HashSet<>());
-        Assert.assertTrue(ratings.get(hashes[0]).equals(12L));
+        Assert.assertEquals(12L, (long) ratings.get(hashes[0]));
     }
 
     //@Test

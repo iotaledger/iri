3784819bdc2f39d4ba2e919ba599cf07942ccf0d
footloosejava <footloosejava@gmail.com>
2018-05-09 10:51:05 -0700

    - added trace level back in. - changed speil to lower case to see if it passes travisCI testing - changed preamble to remove first colon before statement.
    Signed-off-by: footloosejava <32090281+footloosejava@users.noreply.github.com>

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -34,26 +34,26 @@ public class IRI {
 
     private static void configureLogging() {
         String config = System.getProperty("logback.configurationFile");
-        String level = System.getProperty("logging-level", "").toUpperCase();
+        String level = System.getProperty("logging-level", "").toLowerCase();
         switch (level) {
-            case "OFF":
-            case "ERROR":
-            case "WARN":
-            case "INFO":
+            case "off":
+            case "error":
+            case "warn":
+            case "info":
+            case "debug":
                 break;
-            case "DEBUG":
-            case "TRACE":
-            case "ALL":
-                level = "DEBUG";
+            case "trace":
+            case "all":
+                level = "trace";
                 break;
             default:
-                level = "INFO";
+                level = "info";
         }
         System.getProperties().put("logging-level", level);
-        System.out.println("Logging: property 'logging-level' set to : [" + level + "]");
-        System.out.println("Logging: levels can be one of: OFF, ERROR, WARN, INFO, DEBUG");
+        System.out.println("Logging - property 'logging-level' set to: [" + level + "]");
+        System.out.println("Logging - choose from one of these levels: off, error, warn, info, debug, trace");
         if (config != null) {
-            System.out.println("Logging: alternate logging configuration file specified at: '" + config + "'");
+            System.out.println("Logging - alternate logging configuration file specified at: '" + config + "'");
         }
     }
 

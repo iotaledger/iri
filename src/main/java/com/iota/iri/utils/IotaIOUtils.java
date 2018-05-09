23ce209881682896f098cd4f61e358539ea5fa56
galrogo <gal.rogozinski@iota.org>
2018-05-09 16:50:50 +0300

    add debug log

--- src/main/java/com/iota/iri/utils/IotaIOUtils.java
+++ src/main/java/com/iota/iri/utils/IotaIOUtils.java
@@ -2,15 +2,21 @@ package com.iota.iri.utils;
 
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IotaIOUtils extends IOUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(IotaIOUtils.class);
 
     public static void closeQuietly(AutoCloseable... autoCloseables) {
         for (AutoCloseable it : autoCloseables) {
             try {
-                it.close();
+                if (it != null) {
+                    it.close();
+                }
             } catch (Exception ignored) {
-                //ignored
+                log.debug("Silent exception occured", ignored);
             }
         }
     }

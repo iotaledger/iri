8f2b5d2a815ff656de70056e25809c3b38938a58
DyrellC <dyrell.chapman@iota.org>
2018-05-17 15:08:43 -0600

    updated javadocs for interfaces, removed depth limit

--- src/main/java/com/iota/iri/service/tipselection/EntryPoint.java
+++ src/main/java/com/iota/iri/service/tipselection/EntryPoint.java
@@ -1,7 +1,13 @@
 package com.iota.iri.service.tipselection;
 
-
 import com.iota.iri.model.Hash;
+
+/**
+ * This interface is used to enforce usage of the getEntryPoint() method
+ * for selecting the entry point for tip selection. In order to
+ * accurately determine the entry point, a tangle db connection and a
+ * milestone tracking service will be required in the class calls.
+ */
 
 public interface EntryPoint{
 
--- src/main/java/com/iota/iri/service/tipselection/Rating.java
+++ src/main/java/com/iota/iri/service/tipselection/Rating.java
@@ -3,6 +3,12 @@ package com.iota.iri.service.tipselection;
 import com.iota.iri.model.Hash;
 import java.util.Map;
 
+/**
+ * This interface is used to enforce usage of the calculate() method
+ * which is in charge of calculating the cumulative rating of
+ * transactions with connection to the entry point.
+ */
+
 public interface Rating {
 
 

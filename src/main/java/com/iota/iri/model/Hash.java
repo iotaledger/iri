77c4df551933981e0c8d6bb33f776233d681a250
galrogo <gal.rogozinski@iota.org>
2018-05-23 17:59:39 +0300

    HashID changes

--- src/main/java/com/iota/iri/model/Hash.java
+++ src/main/java/com/iota/iri/model/Hash.java
@@ -11,7 +11,7 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Objects;
 
-public final class Hash implements Serializable, Indexable {
+public final class Hash implements Serializable, Indexable, HashId {
 
     public static final int SIZE_IN_TRITS = 243;
     public static final int SIZE_IN_BYTES = 49;

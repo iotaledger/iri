6c32f930c196e96d35f6701664b022fab1498c6b
galrogo <gal.rogozinski@iota.org>
2018-05-24 15:51:33 +0300

    integrate tests

--- src/test/java/com/iota/iri/service/tipselection/impl/WalkerAlphaTest.java
+++ src/test/java/com/iota/iri/service/tipselection/impl/WalkerAlphaTest.java
@@ -2,9 +2,11 @@ package com.iota.iri.service.tipselection.impl;
 
 import com.iota.iri.controllers.TransactionViewModel;
 import com.iota.iri.model.Hash;
+import com.iota.iri.model.HashId;
 import com.iota.iri.service.tipselection.RatingCalculator;
 import com.iota.iri.storage.Tangle;
 import com.iota.iri.storage.rocksDB.RocksDBPersistenceProvider;
+import com.iota.iri.utils.collections.interfaces.TransformingMap;
 import com.iota.iri.zmq.MessageQ;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -67,7 +69,7 @@ public class WalkerAlphaTest {
 
         //calculate rating
         RatingCalculator ratingCalculator = new RatingOne(tangle);
-        Map<Hash, Integer> rating = ratingCalculator.calculate(transaction.getHash());
+        TransformingMap<HashId, Integer> rating = ratingCalculator.calculate(transaction.getHash());
 
         //add 4 after the rating was calculated
         transaction4 = new TransactionViewModel(getRandomTransactionWithTrunkAndBranch(transaction.getHash(),
@@ -104,7 +106,7 @@ public class WalkerAlphaTest {
 
         //calculate rating
         RatingCalculator ratingCalculator = new RatingOne(tangle);
-        Map<Hash, Integer> rating = ratingCalculator.calculate(transaction.getHash());
+        TransformingMap<HashId, Integer> rating = ratingCalculator.calculate(transaction.getHash());
         //set a higher rate for transaction2
         rating.put(transaction2.getHash(), 10);
 
@@ -147,7 +149,7 @@ public class WalkerAlphaTest {
 
         //calculate rating
         RatingCalculator ratingCalculator = new RatingOne(tangle);
-        Map<Hash, Integer> rating = ratingCalculator.calculate(transaction.getHash());
+       TransformingMap<HashId, Integer> rating = ratingCalculator.calculate(transaction.getHash());
         //set a higher rate for transaction2
         rating.put(transaction2.getHash(), 10);
 
@@ -196,7 +198,7 @@ public class WalkerAlphaTest {
 
         //calculate rating
         RatingCalculator ratingCalculator = new RatingOne(tangle);
-        Map<Hash, Integer> rating = ratingCalculator.calculate(transaction.getHash());
+       TransformingMap<HashId, Integer> rating = ratingCalculator.calculate(transaction.getHash());
 
         //reach the tips
         Hash tip = walker.walk(transaction.getHash(), rating, (o -> true));
@@ -223,7 +225,7 @@ public class WalkerAlphaTest {
 
         //calculate rating
         RatingCalculator ratingCalculator = new RatingOne(tangle);
-        Map<Hash, Integer> rating = ratingCalculator.calculate(transaction.getHash());
+       TransformingMap<HashId, Integer> rating = ratingCalculator.calculate(transaction.getHash());
 
         //reach the tips
         Hash tip = walker.walk(transaction.getHash(), rating, (o -> true));
@@ -253,7 +255,7 @@ public class WalkerAlphaTest {
 
         //calculate rating
         RatingCalculator ratingCalculator = new RatingOne(tangle);
-        Map<Hash, Integer> rating = ratingCalculator.calculate(transaction.getHash());
+       TransformingMap<HashId, Integer> rating = ratingCalculator.calculate(transaction.getHash());
 
         //reach the tips
         Hash tip = walker.walk(transaction.getHash(), rating, (o -> true));

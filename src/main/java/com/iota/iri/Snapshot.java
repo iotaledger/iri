ccad485f37fdc2f73524cb861239fa97c5e894bd
Andreas C. Osowski <andreas@osowski.de>
2018-07-09 15:09:33 +0200

    Bump version to 1.5.1

--- src/main/java/com/iota/iri/Snapshot.java
+++ src/main/java/com/iota/iri/Snapshot.java
@@ -20,8 +20,8 @@ public class Snapshot {
     private static final Logger log = LoggerFactory.getLogger(Snapshot.class);
     public static String SNAPSHOT_PUBKEY = "TTXJUGKTNPOOEXSTQVVACENJOQUROXYKDRCVK9LHUXILCLABLGJTIPNF9REWHOIMEUKWQLUOKD9CZUYAC";
     public static int SNAPSHOT_PUBKEY_DEPTH = 6;
-    public static int SNAPSHOT_INDEX = 4;
-    public static int SPENT_ADDRESSES_INDEX = 5;
+    public static int SNAPSHOT_INDEX = 6;
+    public static int SPENT_ADDRESSES_INDEX = 7;
     private static Snapshot initialSnapshot;
 
 

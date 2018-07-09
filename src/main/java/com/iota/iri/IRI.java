67044401d81a5a97b22e76f9c4339285aaf0ced2
Andreas C. Osowski <andreas.chr.osowski@gmail.com>
2018-07-09 16:56:07 +0200

    Update snapshot to 08:00 UTC 20180709 (#855)
    * Update snapshot to 20180709 10:00Z
    
    * Update previousEpochsSpentAddresses to 20180709 10:00Z
    
    * Bump version to 1.5.1
    
    * Update signatures for previousEpochSpentAddresses and snapshotMainnet

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -22,7 +22,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
     public static final String TESTNET_NAME = "IRI Testnet";
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
 
     public static void main(String[] args) throws Exception {
         // Logging is configured first before any references to Logger or LoggerFactory.

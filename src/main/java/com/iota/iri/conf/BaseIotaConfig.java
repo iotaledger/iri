8d32b7c7080b74e9ebc54b9041b92f1684e2b2fb
Gal Rogozinski <galrogogit@gmail.com>
2018-08-19 12:46:35 +0300

    Fix milestone start index default value (#941)
    regression in #910

--- src/main/java/com/iota/iri/conf/BaseIotaConfig.java
+++ src/main/java/com/iota/iri/conf/BaseIotaConfig.java
@@ -665,7 +665,7 @@ public abstract class BaseIotaConfig implements IotaConfig {
         String PREVIOUS_EPOCHS_SPENT_ADDRESSES_TXT = "/previousEpochsSpentAddresses.txt";
         String PREVIOUS_EPOCH_SPENT_ADDRESSES_SIG = "/previousEpochsSpentAddresses.sig";
         long GLOBAL_SNAPSHOT_TIME = 1517180400;
-        int MILESTONE_START_INDEX = 426_550;
+        int MILESTONE_START_INDEX = 590_000;
         int NUM_KEYS_IN_MILESTONE = 20;
         int MAX_ANALYZED_TXS = 20_000;
     }

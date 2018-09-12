21b09ff035d13a367460ad3da8d35f4fd0c6f9b3
Hans Moog <hm@mkjc.net>
2018-09-12 16:19:56 +0200

    Fix: fixed a typo

--- src/main/java/com/iota/iri/utils/dag/TraversalCondition.java
+++ src/main/java/com/iota/iri/utils/dag/TraversalCondition.java
@@ -4,7 +4,7 @@ import com.iota.iri.controllers.TransactionViewModel;
 
 /**
  * Functional interface for the lambda function getting passed into the DAGUtils traverse method to check if the found
- * transactions is relevant for our processing.
+ * transaction is relevant for our processing.
  */
 public interface TraversalCondition {
     boolean check(TransactionViewModel currentTransaction) throws Exception;

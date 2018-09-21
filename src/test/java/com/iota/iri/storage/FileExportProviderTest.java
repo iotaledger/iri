c98821e4e722e3c64a526f021b606252043402ea
Christian Lehmann <info@legacycode.org>
2018-09-21 11:54:03 +0200

    Moved export folder creation dependency from IRI.java to FileExportProvider.java

--- src/test/java/com/iota/iri/storage/FileExportProviderTest.java
+++ src/test/java/com/iota/iri/storage/FileExportProviderTest.java
@@ -3,17 +3,27 @@ package com.iota.iri.storage;
 import com.iota.iri.model.Hash;
 import com.iota.iri.model.Transaction;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
 public class FileExportProviderTest {
-
-    FileExportProvider fileExportProvider;
+    @Rule
+    public TemporaryFolder temporaryFolder = new  TemporaryFolder();
+    private FileExportProvider fileExportProvider;
 
     @Before
     public void setUp() {
         this.fileExportProvider = new FileExportProvider();
+        this.fileExportProvider.init();
     }
 
     @Test
@@ -44,6 +54,26 @@ public class FileExportProviderTest {
         Transaction transaction = new Transaction();
         transaction.sender = "testSender";
         assertFalse(this.fileExportProvider.update(transaction, null, "sender"));
+    }
+
+    @Test
+    public void createDirectory() throws IOException {
+        String tempPath = new File(temporaryFolder.getRoot().getPath(), "testFolder").getPath();
+        Path newFolder = this.fileExportProvider.createDirectory(Paths.get(tempPath));
+        assertTrue(Files.exists(newFolder));
+    }
+
+    @Test
+    public void createDirectoryAlreadyExists() {
+        String tempPath = new File(temporaryFolder.getRoot().getPath(), "testFolderExists").getPath();
+        Path newFolder = this.fileExportProvider.createDirectory(Paths.get(tempPath));
+        Path existingFolder = this.fileExportProvider.createDirectory(newFolder);
+        assertSame(existingFolder, newFolder);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createDirectoryWithNull() {
+        this.fileExportProvider.createDirectory(null);
     }
 
 }

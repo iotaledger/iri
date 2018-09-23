dc8aa3efbe05204a802fec5ca75679c9b6134735
Hans Moog <hm@mkjc.net>
2018-09-23 15:27:18 +0200

    Fix: wrapper reader.close in finally

--- src/main/java/com/iota/iri/service/garbagecollector/GarbageCollector.java
+++ src/main/java/com/iota/iri/service/garbagecollector/GarbageCollector.java
@@ -297,20 +297,22 @@ public class GarbageCollector {
                 )
             );
 
-            String line;
-            while((line = reader.readLine()) != null) {
-                String[] parts = line.split(";", 2);
-                if(parts.length >= 2) {
-                    JobParser jobParser = this.jobParsers.get(parts[0]);
-                    if(jobParser == null) {
-                        throw new GarbageCollectorException("could not determine a parser for cleanup job of type " + parts[0]);
+            try {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    String[] parts = line.split(";", 2);
+                    if(parts.length >= 2) {
+                        JobParser jobParser = this.jobParsers.get(parts[0]);
+                        if(jobParser == null) {
+                            throw new GarbageCollectorException("could not determine a parser for cleanup job of type " + parts[0]);
+                        }
+
+                        addJob(jobParser.parse(parts[1]));
                     }
-
-                    addJob(jobParser.parse(parts[1]));
                 }
+            } finally {
+                reader.close();
             }
-
-            reader.close();
         }
         catch(IOException e) { /* do nothing */ }
         catch(Exception e) {

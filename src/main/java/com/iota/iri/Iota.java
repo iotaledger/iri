419f10ea74925b6371df45594868399046073767
Alon Elmaliah <alon.elmaliah@gmail.com>
2018-10-02 20:25:32 +0300

    enable milestones with different signature modes and security level

--- src/main/java/com/iota/iri/Iota.java
+++ src/main/java/com/iota/iri/Iota.java
@@ -76,7 +76,7 @@ public class Iota {
             tangle.clearColumn(com.iota.iri.model.StateDiff.class);
             tangle.clearMetadata(com.iota.iri.model.persistables.Transaction.class);
         }
-        milestoneTracker.init(SpongeFactory.Mode.CURLP27, ledgerValidator);
+        milestoneTracker.init(SpongeFactory.Mode.CURLP27, 1, ledgerValidator);
         transactionValidator.init(configuration.isTestnet(), configuration.getMwm());
         tipsSolidifier.init();
         transactionRequester.init(configuration.getpRemoveRequest());

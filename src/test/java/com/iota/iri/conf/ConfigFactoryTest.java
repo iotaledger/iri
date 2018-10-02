567bade273cce60d39f2e9414c36ef14ecee8552
Christian Lehmann <info@legacycode.org>
2018-10-02 18:52:34 +0200

    added documentation on unit tests

--- src/test/java/com/iota/iri/conf/ConfigFactoryTest.java
+++ src/test/java/com/iota/iri/conf/ConfigFactoryTest.java
@@ -13,24 +13,38 @@ import java.util.Properties;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
+/**
+ * Tests for the {@link ConfigFactory}
+ */
 public class ConfigFactoryTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
+    /**
+     * Creates and validates a Testnet {@link IotaConfig}.
+     */
     @Test
     public void createIotaConfigTestnet() {
         IotaConfig iotaConfig = ConfigFactory.createIotaConfig(true);
-        assertTrue(iotaConfig instanceof TestnetConfig);
-        assertTrue(iotaConfig.isTestnet());
+        assertTrue("Expected iotaConfig as instance of TestnetConfig.", iotaConfig instanceof TestnetConfig);
+        assertTrue("Expected iotaConfig as Testnet.", iotaConfig.isTestnet());
     }
 
+    /**
+     * Creates and validates a Mainnet {@link IotaConfig}.
+     */
     @Test
     public void createIotaConfigMainnet() {
         IotaConfig iotaConfig = ConfigFactory.createIotaConfig(false);
-        assertTrue(iotaConfig instanceof MainnetConfig);
-        assertFalse(iotaConfig.isTestnet());
+        assertTrue("Expected iotaConfig as instance of MainnetConfig.", iotaConfig instanceof MainnetConfig);
+        assertFalse("Expected iotaConfig as Mainnet.", iotaConfig.isTestnet());
     }
 
+    /**
+     * Creates and validates a Testnet {@link IotaConfig} with <code>TESTNET=true</code> in config file and
+     * <code>testnet: false</code> as method parameter for {@link ConfigFactory#createFromFile(File, boolean)}.
+     * @throws IOException when config file not found.
+     */
     @Test
     public void createFromFileTestnetWithTestnetTrueAndFalse() throws IOException {
         // lets assume in our configFile is TESTNET=true
@@ -38,10 +52,15 @@ public class ConfigFactoryTest {
 
         // but the parameter is set to testnet=false
         IotaConfig iotaConfig = ConfigFactory.createFromFile(configFile, false);
-        assertTrue(iotaConfig instanceof TestnetConfig);
-        assertTrue(iotaConfig.isTestnet());
+        assertTrue("Expected iotaConfig as instance of TestnetConfig.", iotaConfig instanceof TestnetConfig);
+        assertTrue("Expected iotaConfig as Testnet.", iotaConfig.isTestnet());
     }
 
+    /**
+     * Creates and validates a Testnet {@link IotaConfig} with <code>TESTNET=true</code> in config file and
+     * <code>testnet: true</code> as method parameter for {@link ConfigFactory#createFromFile(File, boolean)}.
+     * @throws IOException when config file not found.
+     */
     @Test
     public void createFromFileTestnetWithTestnetTrueAndTrue() throws IOException {
         // lets assume in our configFile is TESTNET=true
@@ -49,10 +68,15 @@ public class ConfigFactoryTest {
 
         // but the parameter is set to testnet=true
         IotaConfig iotaConfig = ConfigFactory.createFromFile(configFile, true);
-        assertTrue(iotaConfig instanceof TestnetConfig);
-        assertTrue(iotaConfig.isTestnet());
+        assertTrue("Expected iotaConfig as instance of TestnetConfig.", iotaConfig instanceof TestnetConfig);
+        assertTrue("Expected iotaConfig as Testnet.", iotaConfig.isTestnet());
     }
 
+    /**
+     * Creates and validates a Testnet {@link IotaConfig} with <code>TESTNET=false</code> in config file and
+     * <code>testnet: true</code> as method parameter for {@link ConfigFactory#createFromFile(File, boolean)}.
+     * @throws IOException when config file not found.
+     */
     @Test
     public void createFromFileTestnetWithTestnetFalseAndTrue() throws IOException {
         // lets assume in our configFile is TESTNET=false
@@ -60,10 +84,15 @@ public class ConfigFactoryTest {
 
         // but the parameter is set to testnet=true
         IotaConfig iotaConfig = ConfigFactory.createFromFile(configFile, true);
-        assertTrue(iotaConfig instanceof TestnetConfig);
-        assertTrue(iotaConfig.isTestnet());
+        assertTrue("Expected iotaConfig as instance of TestnetConfig.", iotaConfig instanceof TestnetConfig);
+        assertTrue("Expected iotaConfig as Testnet.", iotaConfig.isTestnet());
     }
 
+    /**
+     * Creates and validates a Mainnet {@link IotaConfig} with <code>TESTNET=false</code> in config file and
+     * <code>testnet: false</code> as method parameter for {@link ConfigFactory#createFromFile(File, boolean)}.
+     * @throws IOException when config file not found.
+     */
     @Test
     public void createFromFileTestnetWithTestnetFalseAndFalse() throws IOException {
         // lets assume in our configFile is TESTNET=false
@@ -71,10 +100,14 @@ public class ConfigFactoryTest {
 
         // but the parameter is set to testnet=true
         IotaConfig iotaConfig = ConfigFactory.createFromFile(configFile, false);
-        assertTrue(iotaConfig instanceof MainnetConfig);
-        assertFalse(iotaConfig.isTestnet());
+        assertTrue("Expected iotaConfig as instance of MainnetConfig.", iotaConfig instanceof MainnetConfig);
+        assertFalse("Expected iotaConfig as Mainnet.", iotaConfig.isTestnet());
     }
 
+    /**
+     * Try to create an {@link IotaConfig} from a not existing configFile.
+     * @throws IOException when config file not found.
+     */
     @Test(expected = FileNotFoundException.class)
     public void createFromFileTestnetWithFileNotFound() throws IOException {
         File configFile = new File("doesNotExist.ini");
@@ -86,7 +119,7 @@ public class ConfigFactoryTest {
         properties.setProperty("TESTNET", testnet);
         File configFile = folder.newFile("myCustomIotaConfig.ini");
         FileOutputStream fileOutputStream = new FileOutputStream(configFile);
-        properties.store(fileOutputStream, "Testconfig file created by Unit test!");
+        properties.store(fileOutputStream, "Test config file created by Unit test!");
         fileOutputStream.close();
         return configFile;
     }

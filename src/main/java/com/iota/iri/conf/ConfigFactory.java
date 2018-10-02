4ab86dd006ca74344cdebce5cbace81021d02089
Christian Lehmann <info@legacycode.org>
2018-10-02 18:43:43 +0200

    removed unused import

--- src/main/java/com/iota/iri/conf/ConfigFactory.java
+++ src/main/java/com/iota/iri/conf/ConfigFactory.java
@@ -10,7 +10,6 @@ import com.iota.iri.conf.deserializers.CustomBoolDeserializer;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 

e9ec5e43d97e38d00f993ec24f65b59ac9f170ec
Gal Rogozinski <gal.rogozinski@iota.org>
2018-10-02 18:53:17 +0300

    bump to version 1.5.5-RC

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -21,7 +21,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
     public static final String TESTNET_NAME = "IRI Testnet";
-    public static final String VERSION = "1.5.4";
+    public static final String VERSION = "1.5.5_RC";
 
     public static void main(String[] args) throws Exception {
         // Logging is configured first before any references to Logger or LoggerFactory.

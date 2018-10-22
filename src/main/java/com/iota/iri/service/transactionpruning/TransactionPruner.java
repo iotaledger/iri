3aa654ff43c42f732cc1928baaae8846fe443d70
Hans Moog <hm@mkjc.net>
2018-10-22 11:56:08 +0200

    Refactor: cleaned uo unused imports

--- src/main/java/com/iota/iri/service/transactionpruning/TransactionPruner.java
+++ src/main/java/com/iota/iri/service/transactionpruning/TransactionPruner.java
@@ -1,7 +1,5 @@
 package com.iota.iri.service.transactionpruning;
 
-import com.iota.iri.service.snapshot.Snapshot;
-
 /**
  * Represents the manager for the cleanup jobs that are issued by the
  * {@link com.iota.iri.service.snapshot.LocalSnapshotManager} in connection with local snapshots and eventually other
@@ -14,7 +12,7 @@ public interface TransactionPruner {
      *
      * In addition to adding the jobs to the internal list of jobs that have to be executed, it informs the job about
      * the {@link TransactionPruner}, the {@link com.iota.iri.storage.Tangle}, the
-     * {@link com.iota.iri.controllers.TipsViewModel} and the {@link Snapshot} instances
+     * {@link com.iota.iri.controllers.TipsViewModel} and the {@link com.iota.iri.service.snapshot.Snapshot} instances
      * that this job is working on.
      *
      * @param job the job that shall be executed

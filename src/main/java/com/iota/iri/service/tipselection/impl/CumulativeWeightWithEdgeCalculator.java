4753fb9ba130144dc78f7ac23baaf8b8eaa50b94
Zhaoming Yin <stplaydog@gmail.com>
2018-11-07 18:13:23 +0800

    [fix #1122] Remove uneccessary imports

--- src/main/java/com/iota/iri/service/tipselection/impl/CumulativeWeightWithEdgeCalculator.java
+++ src/main/java/com/iota/iri/service/tipselection/impl/CumulativeWeightWithEdgeCalculator.java
@@ -1,19 +1,11 @@
 package com.iota.iri.service.tipselection.impl;
 
-import com.iota.iri.controllers.ApproveeViewModel;
-import com.iota.iri.controllers.TransactionViewModel;
 import com.iota.iri.model.Hash;
 import com.iota.iri.model.HashId;
 import com.iota.iri.model.HashPrefix;
 import com.iota.iri.service.tipselection.RatingCalculator;
-import com.iota.iri.utils.collections.impl.TransformingBoundedHashSet;
-import com.iota.iri.storage.Tangle;
-import com.iota.iri.utils.collections.impl.TransformingMap;
-import com.iota.iri.utils.collections.interfaces.BoundedSet;
 import com.iota.iri.utils.collections.interfaces.UnIterableMap;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.SetUtils;
-import org.apache.commons.lang3.ObjectUtils;
+import com.iota.iri.storage.Tangle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,8 +14,6 @@ import java.util.*;
 /**
  * Implementation of <tt>RatingCalculator</tt> that gives the cumulative for each transaction referencing entryPoint.
  * Used to create a weighted random walks.
- *
- * 
  */
 public class CumulativeWeightWithEdgeCalculator extends CumulativeWeightCalculator {
 

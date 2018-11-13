e8dbe96184afa196b7d201e5d726f571203cc676
Alon Elmaliah <alon.elmaliah@gmail.com>
2018-11-13 14:50:24 +0200

    have all ISS calls use InPlace implementations.

--- src/main/java/com/iota/iri/crypto/ISS.java
+++ src/main/java/com/iota/iri/crypto/ISS.java
@@ -65,25 +65,8 @@ public class ISS {
             throw new RuntimeException("Invalid subseed index: " + index);
         }
 
-        final byte[] subseedPreimage = Arrays.copyOf(seed, seed.length);
-
-        while (index-- > 0) {
-
-            for (int i = 0; i < subseedPreimage.length; i++) {
-
-                if (++subseedPreimage[i] > MAX_TRIT_VALUE) {
-                    subseedPreimage[i] = MIN_TRIT_VALUE;
-                } else {
-                    break;
-                }
-            }
-        }
-
-        final byte[] subseed = new byte[Curl.HASH_LENGTH];
-
-        final Sponge hash = SpongeFactory.create(mode);
-        hash.absorb(subseedPreimage, 0, subseedPreimage.length);
-        hash.squeeze(subseed, 0, subseed.length);
+        final byte[] subseed = Arrays.copyOf(seed, seed.length);
+        subseedInPlace(mode, subseed, index);
         return subseed;
     }
 
@@ -109,9 +92,7 @@ public class ISS {
 
         final byte[] key = new byte[FRAGMENT_LENGTH * numberOfFragments];
 
-        final Sponge hash = SpongeFactory.create(mode);
-        hash.absorb(subseed, 0, subseed.length);
-        hash.squeeze(key, 0, key.length);
+        keyInPlace(mode, subseed, key);
         return key;
     }
 
@@ -135,24 +116,8 @@ public class ISS {
         }
 
         final byte[] digests = new byte[key.length / FRAGMENT_LENGTH * Curl.HASH_LENGTH];
-        final Sponge hash = SpongeFactory.create(mode);
 
-        for (int i = 0; i < key.length / FRAGMENT_LENGTH; i++) {
-
-            final byte[] buffer = Arrays.copyOfRange(key, i * FRAGMENT_LENGTH, (i + 1) * FRAGMENT_LENGTH);
-            for (int j = 0; j < NUMBER_OF_FRAGMENT_CHUNKS; j++) {
-
-                for (int k = MAX_TRYTE_VALUE - MIN_TRYTE_VALUE; k-- > 0;) {
-                    hash.reset();
-                    hash.absorb(buffer, j * Curl.HASH_LENGTH, Curl.HASH_LENGTH);
-                    hash.squeeze(buffer, j * Curl.HASH_LENGTH, Curl.HASH_LENGTH);
-                }
-            }
-            hash.reset();
-            hash.absorb(buffer, 0, buffer.length);
-            hash.squeeze(digests, i * Curl.HASH_LENGTH, Curl.HASH_LENGTH);
-        }
-
+        digestsInPlace(mode, key, digests);
         return digests;
     }
 
@@ -173,10 +138,7 @@ public class ISS {
 
         final byte[] address = new byte[Curl.HASH_LENGTH];
 
-        final Sponge hash = SpongeFactory.create(mode);
-        hash.absorb(digests, 0, digests.length);
-        hash.squeeze(address, 0, address.length);
-
+        addressInPlace(mode, digests, address);
         return address;
     }
 
@@ -201,7 +163,7 @@ public class ISS {
 
         final byte[] normalizedBundle = new byte[Curl.HASH_LENGTH / TRYTE_WIDTH];
 
-        ISSInPlace.normalizedBundle(bundle, normalizedBundle);
+        normalizedBundleInPlace(bundle, normalizedBundle);
         return normalizedBundle;
     }
 
@@ -228,18 +190,9 @@ public class ISS {
             throw new RuntimeException("Invalid key fragment length: " + keyFragment.length);
         }
 
-        final byte[] signatureFragment = Arrays.copyOf(keyFragment, keyFragment.length);
-        final Sponge hash = SpongeFactory.create(mode);
+        final byte[] signatureFragment = new byte[keyFragment.length];
 
-        for (int j = 0; j < NUMBER_OF_FRAGMENT_CHUNKS; j++) {
-
-            for (int k = MAX_TRYTE_VALUE - normalizedBundleFragment[j]; k-- > 0;) {
-                hash.reset();
-                hash.absorb(signatureFragment, j * Curl.HASH_LENGTH, Curl.HASH_LENGTH);
-                hash.squeeze(signatureFragment, j * Curl.HASH_LENGTH, Curl.HASH_LENGTH);
-            }
-        }
-
+        signatureFragmentInPlace(mode, normalizedBundleFragment, keyFragment, signatureFragment);
         return signatureFragment;
     }
 
@@ -273,7 +226,7 @@ public class ISS {
         }
 
         final byte[] digest = new byte[Curl.HASH_LENGTH];
-        ISSInPlace.digest(mode, normalizedBundleFragment, 0, signatureFragment, 0, digest);
+        digestInPlace(mode, normalizedBundleFragment, 0, signatureFragment, 0, digest);
         return digest;
     }
 

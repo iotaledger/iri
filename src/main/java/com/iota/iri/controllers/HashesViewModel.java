240bbbd2ffa1287f4d7e0a2690e64076e9631063
DyrellC <dyrell.chapman@iota.org>
2018-11-14 11:11:58 -0700

    Add Hashes View Model Documentation

--- src/main/java/com/iota/iri/controllers/HashesViewModel.java
+++ src/main/java/com/iota/iri/controllers/HashesViewModel.java
@@ -7,20 +7,33 @@ import com.iota.iri.storage.Indexable;
 import com.iota.iri.storage.Tangle;
 
 /**
- * Base implementation of a controller for hash model objects.
+ * Base implementation of a controller interface for hash model objects.
  */
 public interface HashesViewModel {
 
     /**Stores the given tangle implementation to the controller*/
     boolean store(Tangle tangle) throws Exception;
 
-    /**Variable that containst the size of the object*/
+    /**Returns the size of the model object*/
     int size();
 
     boolean addHash(Hash theHash);
+
+    /**Returns the index of the model reference*/
     Indexable getIndex();
+
+    /**Returns the hash identifiers stored in the model set*/
     Set<Hash> getHashes();
+
+    /**
+     * Removes a referenced object from the database.
+     * @throws Exception If the object does not exist or fails to be removed.
+     */
     void delete(Tangle tangle) throws Exception;
 
+    /**
+     * Returns the next indexed persistable in the database.
+     * @throws Exception If there is no next available persistable
+     */
     HashesViewModel next(Tangle tangle) throws Exception;
 }

bcd3c048629719215464da92b30b52fe2dda2ac0
DyrellC <dyrell.chapman@iota.org>
2018-11-15 23:18:13 -0700

    Add Tag View Model Documentation

--- src/main/java/com/iota/iri/controllers/HashesViewModel.java
+++ src/main/java/com/iota/iri/controllers/HashesViewModel.java
@@ -7,33 +7,35 @@ import com.iota.iri.storage.Indexable;
 import com.iota.iri.storage.Tangle;
 
 /**
- * Base implementation of a controller interface for hash model objects.
+ * Base implementation of a controller interface for sets of {@link com.iota.iri.model.persistables.Hashes}.
  */
 public interface HashesViewModel {
 
-    /**Stores the given tangle implementation to the controller*/
+    /**Store the {@link com.iota.iri.model.persistables.Hashes} set and {@link Hash} reference to the database*/
     boolean store(Tangle tangle) throws Exception;
 
-    /**Returns the size of the model object*/
+    /**Returns the size of the {@link com.iota.iri.model.persistables.Hashes} set*/
     int size();
 
+    /**Add a hash object to the controllers referenced {@link com.iota.iri.model.persistables.Hashes} set*/
     boolean addHash(Hash theHash);
 
-    /**Returns the index of the model reference*/
+    /**Returns the {@link Hash} identifier of the {@link com.iota.iri.model.persistables.Hashes} set*/
     Indexable getIndex();
 
-    /**Returns the hash identifiers stored in the model set*/
+    /**Returns the {@link Hash} identifiers stored in the {@link com.iota.iri.model.persistables.Hashes} set*/
     Set<Hash> getHashes();
 
     /**
-     * Removes a referenced object from the database.
-     * @throws Exception If the object does not exist or fails to be removed.
+     * Removes a referenced {@link com.iota.iri.model.persistables.Hashes} set from the database
+     *
+     * @throws Exception If the {@link com.iota.iri.model.persistables.Hashes} set does not exist or fails to be removed
      */
     void delete(Tangle tangle) throws Exception;
 
     /**
-     * Returns the next indexed persistable in the database.
-     * @throws Exception If there is no next available persistable
+     * Returns the next indexed {@link com.iota.iri.model.persistables.Hashes} set in the database.
+     * @throws Exception If there is no next available {@link com.iota.iri.model.persistables.Hashes} set
      */
     HashesViewModel next(Tangle tangle) throws Exception;
 }

967b166e6cbf8f5d894115ebba184312fb2093d4
Gal Rogozinski <gal.rogozinski@iota.org>
2018-11-18 22:28:01 +0200

    document tangle

--- src/main/java/com/iota/iri/storage/Tangle.java
+++ src/main/java/com/iota/iri/storage/Tangle.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * Created by paul on 3/3/17 for iri.
+ * Delegates methods from {@link PersistenceProvider}
  */
 public class Tangle {
     private static final Logger log = LoggerFactory.getLogger(Tangle.class);
@@ -21,19 +21,27 @@ public class Tangle {
         this.persistenceProviders.add(provider);
     }
 
+    /**
+     * @see PersistenceProvider#init()
+     */
     public void init() throws Exception {
         for(PersistenceProvider provider: this.persistenceProviders) {
             provider.init();
         }
     }
 
-
+    /**
+     * @see PersistenceProvider#shutdown()
+     */
     public void shutdown() throws Exception {
         log.info("Shutting down Tangle Persistence Providers... ");
         this.persistenceProviders.forEach(PersistenceProvider::shutdown);
         this.persistenceProviders.clear();
     }
 
+    /**
+     * @see PersistenceProvider#get(Class, Indexable)
+     */
     public Persistable load(Class<?> model, Indexable index) throws Exception {
             Persistable out = null;
             for(PersistenceProvider provider: this.persistenceProviders) {
@@ -44,6 +52,9 @@ public class Tangle {
             return out;
     }
 
+    /**
+     * @see PersistenceProvider#saveBatch(List)
+     */
     public Boolean saveBatch(List<Pair<Indexable, Persistable>> models) throws Exception {
         boolean exists = false;
         for(PersistenceProvider provider: persistenceProviders) {
@@ -55,6 +66,10 @@ public class Tangle {
         }
         return exists;
     }
+
+    /**
+     * @see PersistenceProvider#save(Persistable, Indexable)
+     */
     public Boolean save(Persistable model, Indexable index) throws Exception {
             boolean exists = false;
             for(PersistenceProvider provider: persistenceProviders) {
@@ -67,18 +82,27 @@ public class Tangle {
             return exists;
     }
 
+    /**
+     * @see PersistenceProvider#deleteBatch(Collection)
+     */
     public void deleteBatch(Collection<Pair<Indexable, ? extends Class<? extends Persistable>>> models) throws Exception {
         for(PersistenceProvider provider: persistenceProviders) {
             provider.deleteBatch(models);
         }
     }
 
+    /**
+     * @see PersistenceProvider#delete(Class, Indexable)
+     */
     public void delete(Class<?> model, Indexable index) throws Exception {
             for(PersistenceProvider provider: persistenceProviders) {
                 provider.delete(model, index);
             }
     }
 
+    /**
+     * @see PersistenceProvider#latest(Class, Class)
+     */
     public Pair<Indexable, Persistable> getLatest(Class<?> model, Class<?> index) throws Exception {
             Pair<Indexable, Persistable> latest = null;
             for(PersistenceProvider provider: persistenceProviders) {
@@ -89,6 +113,9 @@ public class Tangle {
             return latest;
     }
 
+    /**
+     * @see PersistenceProvider#update(Persistable, Indexable, String)
+     */
     public Boolean update(Persistable model, Indexable index, String item) throws Exception {
             boolean success = false;
             for(PersistenceProvider provider: this.persistenceProviders) {
@@ -101,6 +128,9 @@ public class Tangle {
             return success;
     }
 
+    /**
+     * @see PersistenceProvider#keysWithMissingReferences(Class, Class)
+     */
     public Set<Indexable> keysWithMissingReferences(Class<?> modelClass, Class<?> referencedClass) throws Exception {
             Set<Indexable> output = null;
             for(PersistenceProvider provider: this.persistenceProviders) {
@@ -112,6 +142,9 @@ public class Tangle {
             return output;
     }
 
+    /**
+     * @see PersistenceProvider#keysStartingWith(Class, byte[])
+     */
     public Set<Indexable> keysStartingWith(Class<?> modelClass, byte[] value) {
             Set<Indexable> output = null;
             for(PersistenceProvider provider: this.persistenceProviders) {
@@ -123,6 +156,10 @@ public class Tangle {
             return output;
     }
 
+
+    /**
+     * @see PersistenceProvider#exists(Class, Indexable)
+     */
     public Boolean exists(Class<?> modelClass, Indexable hash) throws Exception {
             for(PersistenceProvider provider: this.persistenceProviders) {
                 if (provider.exists(modelClass, hash)) {
@@ -132,6 +169,9 @@ public class Tangle {
             return false;
     }
 
+    /**
+     * @see PersistenceProvider#mayExist(Class, Indexable)
+     */
     public Boolean maybeHas(Class<?> model, Indexable index) throws Exception {
             for(PersistenceProvider provider: this.persistenceProviders) {
                 if (provider.mayExist(model, index)) {
@@ -141,6 +181,9 @@ public class Tangle {
             return false;
     }
 
+    /**
+     * @see PersistenceProvider#count(Class)
+     */
     public Long getCount(Class<?> modelClass) throws Exception {
             long value = 0;
             for(PersistenceProvider provider: this.persistenceProviders) {
@@ -151,6 +194,9 @@ public class Tangle {
             return value;
     }
 
+    /**
+     * @see PersistenceProvider#seek(Class, byte[])
+     */
     public Persistable find(Class<?> model, byte[] key) throws Exception {
             Persistable out = null;
             for (PersistenceProvider provider : this.persistenceProviders) {
@@ -161,6 +207,9 @@ public class Tangle {
             return out;
     }
 
+    /**
+     * @see PersistenceProvider#next(Class, Indexable)
+     */
     public Pair<Indexable, Persistable> next(Class<?> model, Indexable index) throws Exception {
             Pair<Indexable, Persistable> latest = null;
             for(PersistenceProvider provider: persistenceProviders) {
@@ -171,6 +220,9 @@ public class Tangle {
             return latest;
     }
 
+    /**
+     * @see PersistenceProvider#previous(Class, Indexable)
+     */
     public Pair<Indexable, Persistable> previous(Class<?> model, Indexable index) throws Exception {
             Pair<Indexable, Persistable> latest = null;
             for(PersistenceProvider provider: persistenceProviders) {
@@ -181,6 +233,9 @@ public class Tangle {
             return latest;
     }
 
+    /**
+     * @see PersistenceProvider#first(Class, Class)
+     */
     public Pair<Indexable, Persistable > getFirst(Class<?> model, Class<?> index) throws Exception {
             Pair<Indexable, Persistable> latest = null;
             for(PersistenceProvider provider: persistenceProviders) {
@@ -191,29 +246,21 @@ public class Tangle {
             return latest;
     }
 
+    /**
+     * @see PersistenceProvider#clear(Class)
+     */
     public void clearColumn(Class<?> column) throws Exception {
         for(PersistenceProvider provider: persistenceProviders) {
             provider.clear(column);
         }
     }
 
+    /**
+     * @see PersistenceProvider#clearMetadata(Class)
+     */
     public void clearMetadata(Class<?> column) throws Exception {
         for(PersistenceProvider provider: persistenceProviders) {
             provider.clearMetadata(column);
         }
     }
-
-    /*
-    public boolean merge(Persistable model, Indexable index) throws Exception {
-        boolean exists = false;
-        for(PersistenceProvider provider: persistenceProviders) {
-            if(exists) {
-                provider.save(model, index);
-            } else {
-                exists = provider.merge(model, index);
-            }
-        }
-        return exists;
-    }
-    */
 }

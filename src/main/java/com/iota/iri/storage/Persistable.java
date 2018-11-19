cefe1cc236cf632131f6d6a83696bffd0b75d7c0
Gal Rogozinski <gal.rogozinski@iota.org>
2018-11-19 19:12:09 +0200

    document persistable and indexable

--- src/main/java/com/iota/iri/storage/Persistable.java
+++ src/main/java/com/iota/iri/storage/Persistable.java
@@ -2,10 +2,49 @@ package com.iota.iri.storage;
 
 import java.io.Serializable;
 
+/**
+ * Any object that can be serialized and stored in the DB.
+ *
+ * @see Indexable
+ */
 public interface Persistable extends Serializable {
+    /**
+     * Serializes the core fields of the object to bytes that can be stored in a database. Core fields are all the
+     * fields that represent the object in other nodes in the network.
+     *
+     * @return the serialized bytes that represents the core original object
+     */
     byte[] bytes();
+
+    /**
+     * Recreates the core state (populates the fields) of the object by deserializing {@code bytes}.
+     *
+     * @param bytes the serialized version the object
+     */
     void read(byte[] bytes);
+
+    /**
+     * Serializes fields that usually contain data about the object that the node itself calculates
+     * and is not passed to different nodes. This data is supposed to be stored in a different column family/table then
+     * the data serialized by {@link #bytes()}.
+     *
+     * @return the serialized bytes that represents the metadata of the object
+     */
     byte[] metadata();
+
+    /**
+     * Recreates the metadata (populates the relevant fields) of the object by deserializing {@code bytes}.
+     *
+     * @param bytes the serialized bytes that represents the metadata of the object
+     */
     void readMetadata(byte[] bytes);
+
+    /**
+     * Specifies whether the object should be appended to an existing key/index or should it replace the old
+     * indexed object.
+     *
+     * @return <tt>true</tt> if we should add the object to a list for an existing key. <tt>false</tt> if we should
+     * false if we should replace the old value
+     */
     boolean merge();
 }

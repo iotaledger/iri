df636c27202ac7538974c1562c0cd66258033aee
Brord van Wierst <brord@brord.net>
2018-11-20 16:51:44 +0200

    Apply suggestions from code review
    Co-Authored-By: GalRogozinski <galrogogit@gmail.com>

--- src/main/java/com/iota/iri/storage/Persistable.java
+++ src/main/java/com/iota/iri/storage/Persistable.java
@@ -3,14 +3,14 @@ package com.iota.iri.storage;
 import java.io.Serializable;
 
 /**
- * Any object that can be serialized and stored in the DB.
+ * Any object that can be serialized and stored in the database.
  *
  * @see Indexable
  */
 public interface Persistable extends Serializable {
     /**
      * Serializes the core fields of the object to bytes that can be stored in a database. Core fields are all the
-     * fields that represent the object in other nodes in the network.
+     * fields that represent the core data required to construct this object.
      *
      * @return the serialized bytes that represents the core original object
      */
@@ -19,13 +19,13 @@ public interface Persistable extends Serializable {
     /**
      * Recreates the core state (populates the fields) of the object by deserializing {@code bytes}.
      *
-     * @param bytes the serialized version the object
+     * @param bytes the serialized version of the object
      */
     void read(byte[] bytes);
 
     /**
      * Serializes fields that usually contain data about the object that the node itself calculates
-     * and is not passed to different nodes. This data is supposed to be stored in a different column family/table then
+     * and is not passed to different nodes. This data is stored in a different column family/table then
      * the data serialized by {@link #bytes()}.
      *
      * @return the serialized bytes that represents the metadata of the object
@@ -40,7 +40,7 @@ public interface Persistable extends Serializable {
     void readMetadata(byte[] bytes);
 
     /**
-     * Specifies whether the object should be appended to an existing key/index or should it replace the old
+     * Specifies whether the object should be appended to an existing key/index or should replace the old
      * indexed object.
      *
      * @return <tt>true</tt> if we should add the object to a list for an existing key. <tt>false</tt> if we should

75d47a722f9015fbe718530de416fafb3e120359
Hans Moog <hm@mkjc.net>
2018-11-30 11:04:10 +0100

    Refactor: reverted accidental change

--- src/main/java/com/iota/iri/service/TipsSolidifier.java
+++ src/main/java/com/iota/iri/service/TipsSolidifier.java
@@ -63,7 +63,7 @@ public class TipsSolidifier {
                 tipsViewModel.removeTipHash(hash);
                 isTip = false;
             }
-            if (hash != null && isTip && transactionValidator.checkSolidity(hash, false, 10)) {
+            if (hash != null && isTip && transactionValidator.checkSolidity(hash, false)) {
                 //if(hash != null && TransactionViewModel.fromHash(hash).isSolid() && isTip) {
                 tipsViewModel.setSolid(hash);
             }

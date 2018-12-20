f596b023ff524bf3e488f7a8132ef85b2fb92aeb
Gal Rogozinski <gal.rogozinski@iota.org>
2018-12-20 10:50:08 +0200

    Bump to version 1.6.0-RC10

--- src/main/java/com/iota/iri/IRI.java
+++ src/main/java/com/iota/iri/IRI.java
@@ -42,7 +42,7 @@ public class IRI {
 
     public static final String MAINNET_NAME = "IRI";
     public static final String TESTNET_NAME = "IRI Testnet";
-    public static final String VERSION = "1.6.0-RC9";
+    public static final String VERSION = "1.6.0-RC10";
 
     /**
      * The entry point of IRI.

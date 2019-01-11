ffeee1ed584d9e603db774b40d81d7c2e5228b76
tylerw1369 <39392335+tylerw1369@users.noreply.github.com>
2019-01-10 19:21:05 -0700

    Update APIConfig.java

--- src/main/java/com/iota/iri/conf/APIConfig.java
+++ src/main/java/com/iota/iri/conf/APIConfig.java
@@ -48,6 +48,11 @@ public interface APIConfig extends Config {
      * @return {@value Descriptions#REMOTE_AUTH}
      */
     String getRemoteAuth();
+    
+    /**
+     * @return {@value Descriptions#EXTERNAL_POW_LIB}
+     */
+    String getExternalPoWLib();
 
     interface Descriptions {
         String PORT = "The port that will be used by the API.";
@@ -59,5 +64,6 @@ public interface APIConfig extends Config {
         String MAX_GET_TRYTES = "The maximal number of trytes that may be returned by the \"getTrytes\" API call. If the number of transactions found exceeds this number an error will be returned.";
         String MAX_BODY_LENGTH = "The maximal number of characters the body of an API call may hold. If a request body length exceeds this number an error will be returned.";
         String REMOTE = "Open the API interface to any host. Equivalent to \"--api-host 0.0.0.0\"";
+        String EXTERNAL_POW_LIB = "Path to the external PearlDiver Library";
     }
 }

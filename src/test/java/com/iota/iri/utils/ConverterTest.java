299cf1194b0bb2c2a7472c82f4bc38c59fe58353
lhf <lhfbc@163.com>
2019-01-29 16:02:04 +0800

    add one more test case.

--- src/test/java/com/iota/iri/utils/ConverterTest.java
+++ src/test/java/com/iota/iri/utils/ConverterTest.java
@@ -8,6 +8,9 @@ public class ConverterTest {
     public void testTrytesToAscii() {
         String trytes = "OBGCZBOBMBCCOBNBNCACOBBCDCVBCC9";
         Assert.assertEquals(Converter.trytesToAscii(trytes), "EXPECTED_RESULT\0");
+
+        trytes = "RBOBVBVBYB999999999";
+        Assert.assertEquals(Converter.trytesToAscii(trytes), "HELLO\0\0\0\0\0");
     }
 
 }

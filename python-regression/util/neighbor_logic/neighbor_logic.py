e61bf5181b4a593602b80a58fe636204d8aba6c2
DyrellC <dyrell.chapman@iota.org>
2019-02-20 22:23:19 -0700

    Fix neighbor logic bug

--- python-regression/util/neighbor_logic/neighbor_logic.py
+++ python-regression/util/neighbor_logic/neighbor_logic.py
@@ -17,8 +17,8 @@ def check_if_neighbors(api, neighbors, expected_neighbor):
     """
     is_neighbor = False
 
-    for neighbor in enumerate(neighbors):
-        if expected_neighbor == neighbor['address']:
+    for neighbor_index in range(len(neighbors)):
+        if expected_neighbor == neighbors[neighbor_index]['address']:
             logger.info("Already a neighbor")
             is_neighbor = True
         else:

aca2a65f317cf084923885ea25b2363203c7a01f
Christian Lehmann <info@legacycode.org>
2019-03-05 18:28:10 +0100

    Return ip address instead of hostname.

--- src/main/java/com/iota/iri/service/dto/GetNeighborsResponse.java
+++ src/main/java/com/iota/iri/service/dto/GetNeighborsResponse.java
@@ -2,12 +2,10 @@ package com.iota.iri.service.dto;
 
 import java.util.List;
 
-import com.iota.iri.service.API;
-
 /**
  * 
  * Contains information about the result of a successful {@code getNeighbors} API call.
- * See {@link API#getNeighborsStatement} for how this response is created.
+ * See {@link GetNeighborsResponse#create(List)} for how this response is created.
  *
  */
 public class GetNeighborsResponse extends AbstractResponse {
@@ -25,7 +23,7 @@ public class GetNeighborsResponse extends AbstractResponse {
      *     <li>numberOfSentTransactions</li>
      *     <li>numberOfStaleTransactions</li>
      * </ol>
-     * @see {@link com.iota.iri.service.dto.GetNeighborsResponse.Neighbor}
+     * @see com.iota.iri.service.dto.GetNeighborsResponse.Neighbor
      */
     private Neighbor[] neighbors;
 
@@ -58,16 +56,17 @@ public class GetNeighborsResponse extends AbstractResponse {
      * A plain DTO of an iota neighbor.
      * 
      */
+    @SuppressWarnings("unused")
     static class Neighbor {
 
         private String address;
-        public long numberOfAllTransactions,
-                numberOfRandomTransactionRequests,
-                numberOfNewTransactions,
-                numberOfInvalidTransactions,
-                numberOfStaleTransactions,
-                numberOfSentTransactions;
-        public String connectionType;
+        long numberOfAllTransactions;
+        long numberOfRandomTransactionRequests;
+        long numberOfNewTransactions;
+        long numberOfInvalidTransactions;
+        long numberOfStaleTransactions;
+        long numberOfSentTransactions;
+        String connectionType;
 
         /**
          * The address of your neighbor
@@ -139,10 +138,10 @@ public class GetNeighborsResponse extends AbstractResponse {
          * @param n the neighbor currently connected to this node
          * @return a new instance of {@link GetNeighborsResponse.Neighbor}
          */
-        public static Neighbor createFrom(com.iota.iri.network.Neighbor n) {
+        static Neighbor createFrom(com.iota.iri.network.Neighbor n) {
             Neighbor ne = new Neighbor();
             int port = n.getPort();
-            ne.address = n.getAddress().getHostString() + ":" + port;
+            ne.address = n.getAddress().getAddress().getHostAddress() + ":" + port;
             ne.numberOfAllTransactions = n.getNumberOfAllTransactions();
             ne.numberOfInvalidTransactions = n.getNumberOfInvalidTransactions();
             ne.numberOfStaleTransactions = n.getNumberOfStaleTransactions();

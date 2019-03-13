d4f2a50324eb8ad9a79a54f6bfb7f7089e056db9
Gal Rogozinski <galrogogit@gmail.com>
2019-03-13 09:09:38 +0100

    Change: Prepare IRI to have its version injected by CI (#1359)
    * Parse version from jar manifest
    
    * Parse version from pom when running directly from IDE
    
    * update pom version
    
    * add commit hash to version
    
    * get correct version wiht travis
    
    * delete unneccessary comment
    
    *  change travis.yml to release properly
    
    * add doc and delete unused imports

--- src/main/java/com/iota/iri/utils/IotaUtils.java
+++ src/main/java/com/iota/iri/utils/IotaUtils.java
@@ -1,9 +1,15 @@
 package com.iota.iri.utils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.iota.iri.IRI;
 import com.iota.iri.model.Hash;
 
+import java.io.FileReader;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -16,6 +22,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IotaUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(IotaUtils.class);
+
+    /**
+     * Returns the current version IRI is running by reading the Jar manifest.
+     * If we run not from a jar or the manifest is missing we read straight from the pom
+     *
+     * @return the implementation version of IRI
+     */
+    public static String getIriVersion() {
+        String implementationVersion = IRI.class.getPackage().getImplementationVersion();
+        //If not in manifest (can happen when running from IDE)
+        if (implementationVersion == null) {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            try {
+                Model model = reader.read(new FileReader("pom.xml"));
+                implementationVersion = model.getVersion();
+            } catch (Exception e) {
+                log.error("Failed to parse version from pom", e);
+            }
+        }
+        return implementationVersion;
+    }
 
     public static List<String> splitStringToImmutableList(String string, String regexSplit) {
         return Arrays.stream(string.split(regexSplit))

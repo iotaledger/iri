f4c2f0922fdda452f5366e5eab7642393185a33d
Brord van Wierst <brord@iota.org>
2019-04-29 23:34:06 +0200

    Removed unused var

--- src/main/java/com/iota/iri/service/spentaddresses/impl/SpentAddressesProviderImpl.java
+++ src/main/java/com/iota/iri/service/spentaddresses/impl/SpentAddressesProviderImpl.java
@@ -61,8 +61,6 @@ public class SpentAddressesProviderImpl implements SpentAddressesProvider {
             return;
         }
 
-        String test = config.getPreviousEpochSpentAddressesFiles();
-        
         for (String previousEpochsSpentAddressesFile : config.getPreviousEpochSpentAddressesFiles().split(" ")) {
                 readSpentAddressesFromStream(
                         SpentAddressesProviderImpl.class.getResourceAsStream(previousEpochsSpentAddressesFile));

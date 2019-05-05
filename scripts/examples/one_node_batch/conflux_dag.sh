9dd3f7cc9709908e757141045a28c198ba4232d6
Zhaoming Yin <stplaydog@gmail.com>
2019-05-05 17:48:45 +0800

    remove redundant code

--- scripts/examples/one_node_batch/conflux_dag.sh
+++ scripts/examples/one_node_batch/conflux_dag.sh
@@ -22,6 +22,4 @@ java -jar iri-1.5.5.jar --testnet \
                         --ipfs-txns false \
                         --batch-txns true \
                         --weight-calculation-algorithm "IN_MEM" \
-                        --ancestor-forward-enable true  \
-                        --ancestor-create-frequency 100  \
                         &>  streamnet.log &

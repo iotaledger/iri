bac266ef494c97bacb81af6dbba23dce49ac22d9
niming2100 <niming_2009@163.com>
2019-05-06 11:33:22 +0800

    [fix #352]1.add DevOPS page;2.DevOps url can be configed

---
+++ scripts/front_end/web/src/views/nav1/DevOps.vue
@@ -0,0 +1,18 @@
+<template>
+    <iframe v-bind:src="opsUrl"></iframe>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {opsUrl: this.ConfigUrls.OPS}
+        },
+        methods: {}
+    }
+</script>
+<style>
+    iframe {
+        width: 85vw;
+        height: 50vw;
+    }
+</style>

99923e9bc5ea72ae13cc417c9c5a9750a848ca2a
Zhaoming Yin <stplaydog@gmail.com>
2019-05-14 09:31:56 +0800

    [fix #50] Support Crypto

---
+++ scripts/key_manager/aes.py
@@ -0,0 +1,35 @@
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+import sys
+
+class AESCipher(object):
+
+    def __init__(self, key): 
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+msg = sys.argv[1]
+aes = AESCipher("7ba5250258b9c90cb6cfa0dfbaeaf0b465443cc49b6eef78f87ca339c71757fd")
+aes_str = aes.encrypt(msg)
+print aes_str

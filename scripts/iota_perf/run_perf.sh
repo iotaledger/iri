a04a487dab683300e6311713cc842c0a1d504b30
lhfbc <lhf_bc@163.com>
2019-06-03 19:19:44 +0800

    modify sed

--- scripts/iota_perf/run_perf.sh
+++ scripts/iota_perf/run_perf.sh
@@ -1,25 +1,31 @@
-# deploy environment (specifically nginx)
+#!/usr/bin/env bash
+
+# start iota
+
+# start cli
+
+# start nginx
 #sudo cp conf_info/nginx/aws/nginx3.conf /etc/nginx/nginx.conf
 
 # generate data
 python data_generate.py 10000
 
 # run ramp up
-sed 's/NUM_CALL/500/g' PerformanceTestDAG2TM_TPS.jmx | tee   PerformanceTest.jmx >  /dev/null
-sed 's/NUM_THREAD/1/g' PerformanceTest.jmx | tee   PerformanceTest1.jmx > /dev/null
-sed 's/PORT/5000/g' PerformanceTest1.jmx | tee   PerformanceTest.jmx > /dev/null
-sed 's/DATA/data/g' PerformanceTest.jmx | tee   PerformanceTest1.jmx > /dev/null
-sed 's/ACTION/put_file/g' PerformanceTest1.jmx | tee PerformanceTest.jmx > /dev/null
+sed -e 's/NUM_CALL/500/g'       \
+    -e 's/NUM_THREAD/1/g'       \
+    -e 's/PORT/5000/g'          \
+    -e 's/DATA/data/g'          \
+    -e 's/ACTION/put_file/g'   PerformanceTestDAG2TM_TPS.jmx > PerformanceTest.jmx
 jmeter -n -t PerformanceTest.jmx
 
 sleep 10
 
 # run perf with multi-threading
-sed 's/NUM_CALL/5000/g' PerformanceTestDAG2TM_TPS.jmx | tee   PerformanceTest.jmx >  /dev/null
-sed 's/NUM_THREAD/2/g' PerformanceTest.jmx | tee   PerformanceTest1.jmx > /dev/null
-sed 's/PORT/8080/g' PerformanceTest1.jmx | tee   PerformanceTest.jmx > /dev/null
-sed 's/DATA/data1/g' PerformanceTest.jmx | tee   PerformanceTest1.jmx > /dev/null
-sed 's/ACTION/put_cache/g' PerformanceTest1.jmx | tee PerformanceTest.jmx > /dev/null
+sed -e 's/NUM_CALL/5000/g'  \
+    -e 's/NUM_THREAD/2/g'   \
+    -e 's/PORT/8080/g'      \
+    -e 's/DATA/data1/g'     \
+    -e 's/ACTION/put_cache/g' PerformanceTestDAG2TM_TPS.jmx > PerformanceTest1.jmx
 jmeter -n -t PerformanceTest.jmx
 
 sleep 30

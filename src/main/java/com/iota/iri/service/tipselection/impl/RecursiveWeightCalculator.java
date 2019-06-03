676eef4994f91fc627af83c0ac6d36663c5d6b09
Brord van Wierst <brord@iota.org>
2019-06-03 19:23:29 +0200

    Added some map optimizations and decreased db usage

--- src/main/java/com/iota/iri/service/tipselection/impl/RecursiveWeightCalculator.java
+++ src/main/java/com/iota/iri/service/tipselection/impl/RecursiveWeightCalculator.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import org.apache.commons.collections4.CollectionUtils;
 
 import com.iota.iri.controllers.ApproveeViewModel;
+import com.iota.iri.controllers.TransactionViewModel;
 import com.iota.iri.model.Hash;
 import com.iota.iri.model.HashId;
 import com.iota.iri.model.HashPrefix;
@@ -26,7 +27,7 @@ import com.iota.iri.utils.collections.interfaces.UnIterableMap;
  */
 public class RecursiveWeightCalculator implements RatingCalculator {
 
-    public final Tangle tangle;
+    private final Tangle tangle;
     private final SnapshotProvider snapshotProvider;
     
     private Map<Hash, ArrayDeque<Hash>> txToDirectApprovers = new HashMap<>();
@@ -43,17 +44,20 @@ public class RecursiveWeightCalculator implements RatingCalculator {
 
     @Override
     public UnIterableMap<HashId, Integer> calculate(Hash entryPoint) throws Exception {
-        Set<Hash> toCheck = new HashSet<>(1);
-        toCheck.add(entryPoint);
-        
-        // Initial capacity of 16, as default for java maps and lists
-        UnIterableMap<HashId, Integer> hashWeight = createTxHashToCumulativeWeightMap(16);
-        calculateRatingDfs(entryPoint, hashWeight);
+        UnIterableMap<HashId, Integer> hashWeight = calculateRatingDfs(entryPoint);
         
         return hashWeight;
     }
     
-    private void calculateRatingDfs(Hash entryPoint, UnIterableMap<HashId, Integer> hashWeight) throws Exception {
+    private UnIterableMap<HashId, Integer> calculateRatingDfs(Hash entryPoint) throws Exception {
+        TransactionViewModel tvm = TransactionViewModel.fromHash(tangle, entryPoint);
+        int depth = tvm.snapshotIndex() > 0 
+                ? snapshotProvider.getLatestSnapshot().getIndex() - tvm.snapshotIndex() + 1 
+                : 1;
+
+        // Estimated capacity per depth, assumes 5 minute gap in between milestones, at 3tps
+        UnIterableMap<HashId, Integer> hashWeight = createTxHashToCumulativeWeightMap( 5 * 60 * 3 * depth);
+        
         Deque<Hash> stack = new ArrayDeque<>();
         Map<Hash, ArrayDeque<Hash>> txToDirectApprovers = new HashMap<>();
 
@@ -61,28 +65,30 @@ public class RecursiveWeightCalculator implements RatingCalculator {
         while (CollectionUtils.isNotEmpty(stack)) {
             Hash txHash = stack.peek();
             if (!hashWeight.containsKey(txHash)) {
-                Collection<Hash> appHashes = getTxDirectApproversHashes(txHash, txToDirectApprovers);
+                Collection<Hash> appHashes = getTxDirectApproversHashes(txHash, txToDirectApprovers, txToDirectApprovers);
                 if (CollectionUtils.isNotEmpty(appHashes)) {
                     Hash txApp = getAndRemoveApprover(appHashes);
                     stack.push(txApp);
                     continue;
                 }
-            }
-            else {
+            } else {
                 stack.pop();
                 continue;
             }
             
             HashSet<HashId> set = new HashSet<>();
             set.add(txHash);
-            hashWeight.put(txHash, getRating(txHash, set));
+            
+            int rating = txHash.equals(entryPoint) ? hashWeight.size() + 1 : getRating(txHash, set);
+            hashWeight.put(txHash, rating);
         }
+        return hashWeight;
     }
 
     private int getRating(Hash hash, Set<HashId> seenHashes) throws Exception {
         int weight = 1;
 
-        ArrayDeque<Hash> approvers = getTxDirectApproversHashes(hash, txToDirectApprovers);
+        ArrayDeque<Hash> approvers = getTxDirectApproversHashes(hash, txToDirectApprovers, null);
         for (Hash approver : approvers) {
             if (!seenHashes.contains(approver)) {
                 seenHashes.add(approver);
@@ -100,21 +106,37 @@ public class RecursiveWeightCalculator implements RatingCalculator {
         return txApp;
     }
     
-    private ArrayDeque<Hash> getTxDirectApproversHashes(Hash txHash,  Map<Hash, ArrayDeque<Hash>> txToDirectApprovers) 
+    /**
+     * Finds the approvers of a transaction, and adds it to the txToDirectApprovers map if they werent there yet.
+     * 
+     * @param txHash The tx we find the approvers of
+     * @param txToDirectApprovers The map we look in, and add to
+     * @param fallback The map we check in before going in the database, can be <code>null</code>
+     * @return
+     * @throws Exception
+     */
+    private ArrayDeque<Hash> getTxDirectApproversHashes(Hash txHash,  
+            Map<Hash, ArrayDeque<Hash>> txToDirectApprovers, 
+            Map<Hash, ArrayDeque<Hash>> fallback) 
             throws Exception {
         
         ArrayDeque<Hash> txApprovers = txToDirectApprovers.get(txHash);
         if (txApprovers == null) {
-            ApproveeViewModel approvers = ApproveeViewModel.load(tangle, txHash);
-            Collection<Hash> appHashes = CollectionUtils.emptyIfNull(approvers.getHashes());
-            txApprovers = new ArrayDeque<>(appHashes.size());
-            for (Hash appHash : appHashes) {
-                //if not genesis (the tx that confirms itself)
-                if (!snapshotProvider.getInitialSnapshot().hasSolidEntryPoint(appHash)) {
-                    txApprovers.add(appHash);
+            if (fallback != null && fallback.containsKey(txHash)) {
+                txApprovers = fallback.get(txHash);
+                txToDirectApprovers.put(txHash, txApprovers);
+            } else {
+                ApproveeViewModel approvers = ApproveeViewModel.load(tangle, txHash);
+                Collection<Hash> appHashes = CollectionUtils.emptyIfNull(approvers.getHashes());
+                txApprovers = new ArrayDeque<>(appHashes.size());
+                for (Hash appHash : appHashes) {
+                    //if not genesis (the tx that confirms itself)
+                    if (!snapshotProvider.getInitialSnapshot().hasSolidEntryPoint(appHash)) {
+                        txApprovers.add(appHash);
+                    }
                 }
+                txToDirectApprovers.put(txHash, txApprovers);
             }
-            txToDirectApprovers.put(txHash, txApprovers);
         }
         return txApprovers;
     }

306230ede390909bc8c368c8d96968a3d99d326c
wangyh2016 <850138226@qq.com>
2019-06-21 15:39:46 +0800

    [# 91] create a new folder about mysql dockerfile (#117)

--- scripts/front_end/run_oauth_docker.sh
+++ scripts/front_end/run_oauth_docker.sh
@@ -6,5 +6,5 @@ sudo docker build -t ${OAUTH_NAME}:${TAG} --build-arg HOST_IP=$1 .
 sudo docker run -itd -p 80:80 -p 8000:8000 -p 9081:9081 -p 9080:9080  --name ${OAUTH_NAME} ${OAUTH_NAME}:${TAG}
 cd trias-oauth/
 MYSQL_NAME=mysql
-sudo docker build -t ${MYSQL_NAME}:${TAG} .
+sudo docker build -t ${MYSQL_NAME}:${TAG} -f mysql_docker/Dockerfile .
 sudo docker run -itd -p 3306:3306 --name ${MYSQL_NAME} ${MYSQL_NAME}:${TAG}

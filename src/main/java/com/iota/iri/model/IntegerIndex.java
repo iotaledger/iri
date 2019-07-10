fe9b03c30b0074f2d1f5be7a2f800e7db3f07c82
Dyrell Chapman <dyrell.chapman@iota.org>
2019-07-10 14:59:59 +0200

    Documentation: Models Documentation (#1144)
    * Document models/safe
    
    * Document Persistables
    
    * Document hash models
    
    * Update hash types documentation
    
    * Update persistables docs
    
    * Update src/main/java/com/iota/iri/model/IntegerIndex.java
    
    Co-Authored-By: DyrellC <dyrell.chapman@iota.org>
    
    * Hash documentation adjustment, persistables documentation correction
    
    * Update src/main/java/com/iota/iri/model/ObsoleteTagHash.java
    
    Co-Authored-By: DyrellC <dyrell.chapman@iota.org>
    
    * Add missing overrides
    
    * Reword persistable class desc, reorganizing
    
    * Update null hash description
    
    * Remove Unnecessary docs

--- src/main/java/com/iota/iri/model/IntegerIndex.java
+++ src/main/java/com/iota/iri/model/IntegerIndex.java
@@ -3,14 +3,25 @@ package com.iota.iri.model;
 import com.iota.iri.storage.Indexable;
 import com.iota.iri.utils.Serializer;
 
+/**
+ An integer key that indexes {@link Persistable} objects in the database.
+ */
 public class IntegerIndex implements Indexable{
+
+    /**The internally stored index value*/
     int value;
+
+    /**
+     * Constructor for storing index of persistable
+     * @param value The index of the represented persistable
+     */
     public IntegerIndex(int value) {
         this.value = value;
     }
 
     public IntegerIndex() {}
 
+    /**@return The index of the persistable */
     public int getValue() {
         return value;
     }
@@ -25,11 +36,13 @@ public class IntegerIndex implements Indexable{
         this.value = Serializer.getInteger(bytes);
     }
 
+    /**Creates a new {@link #IntegerIndex(int)} with an incremented index value*/
     @Override
     public Indexable incremented() {
         return new IntegerIndex(value + 1);
     }
 
+    /**Creates a new {@link #IntegerIndex(int)} with a decremented index value*/
     @Override
     public Indexable decremented() {
         return new IntegerIndex(value - 1);

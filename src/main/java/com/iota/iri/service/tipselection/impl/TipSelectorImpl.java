04ae27c3123d0dd826e468c1abce3b0fbf6dff3d
Gal Rogozinski <gal.rogozinski@iota.org>
2019-09-11 17:25:39 +0300

    fix condition

--- src/main/java/com/iota/iri/service/tipselection/impl/TipSelectorImpl.java
+++ src/main/java/com/iota/iri/service/tipselection/impl/TipSelectorImpl.java
@@ -129,11 +129,11 @@ public class TipSelectorImpl implements TipSelector {
 
     private void checkReference(Hash reference, Map<Hash, Integer> rating, WalkValidator walkValidator)
             throws Exception {
-        if (config.getAlpha() == 0 && !walkValidator.isValid(reference)) {
-            throw new InvalidAlgorithmParameterException(REFERENCE_TRANSACTION_IS_INVALID);
-        }
-        else if (!rating.containsKey(reference)) {
+        if (config.getAlpha() != 0 && !rating.containsKey(reference)) {
             throw new InvalidAlgorithmParameterException(REFERENCE_TRANSACTION_TOO_OLD);
+        }
+        else if (config.getAlpha() == 0 && !walkValidator.isValid(reference)) {
+            throw new InvalidAlgorithmParameterException(REFERENCE_TRANSACTION_IS_INVALID);
         }
     }
 }

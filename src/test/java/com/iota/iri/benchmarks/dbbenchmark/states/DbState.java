1336af5e5993b87f7798c3e1ed21e74245ed4ea0
DyrellC <dyrell.chapman@iota.org>
2019-11-17 21:53:08 -0700

    Missing import

--- src/test/java/com/iota/iri/benchmarks/dbbenchmark/states/DbState.java
+++ src/test/java/com/iota/iri/benchmarks/dbbenchmark/states/DbState.java
@@ -3,6 +3,7 @@ package com.iota.iri.benchmarks.dbbenchmark.states;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;

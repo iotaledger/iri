6d66e61131e577fb8b05fbc3395a5d53106ca767
Gal Rogozinski <gal.rogozinski@iota.org>
2020-05-25 14:49:20 +0300

    Fix: resolve race in networking layer assigning write operation to key (#1865)
    * Revert "Fix: Remove condition in send (#1864)"
    
    This reverts commit d6041bcb9cb4051b44db1f9283d3ede351486bd1.
    
    * Synchronize interest ops on selector keys
    
    * fix null-pointer exception in test

--- src/main/java/com/iota/iri/network/NeighborRouterImpl.java
+++ src/main/java/com/iota/iri/network/NeighborRouterImpl.java
@@ -355,8 +355,7 @@ public class NeighborRouterImpl implements NeighborRouter {
     private boolean handleWrite(SocketChannel channel, SelectionKey key, String identity, Neighbor neighbor) {
         try {
             switch (neighbor.write()) {
-                case 0:
-                    break;
+                // something bad happened
                 case -1:
                     if (neighbor.getState() == NeighborState.HANDSHAKING) {
                         log.info("closing connection to {} as handshake packet couldn't be written", identity);
@@ -365,8 +364,14 @@ public class NeighborRouterImpl implements NeighborRouter {
                         closeNeighborConnection(channel, identity, selector);
                     }
                     return false;
+                // bytes were either written or not written to the channel
+                // we check whether we still have something else to send, if not we unregister write
                 default:
-                    // bytes were written to the channel
+                    synchronized (key) {
+                        if (!neighbor.hasDataToSendTo()) {
+                            key.interestOps(SelectionKey.OP_READ);
+                        }
+                    }
             }
             return true;
         } catch (IOException ex) {
